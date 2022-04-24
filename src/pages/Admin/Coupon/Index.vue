37982f4c45fcc01f530e593f8f88a1cfa9132ede
Nima.mr <nima1380620@yahoo.com>
2022-04-24 14:08:49 +0430

    first commit

---
+++ src/pages/Admin/Coupon/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <entity-index
+    v-model:value="inputs"
+    title="لیست بن ها"
+    :api="api"
+    :table="table"
+    :table-keys="tableKeys"
+    :create-route-name="createRouteName">
+  </entity-index>
+</template>
+
+<script>
+import { EntityIndex } from 'quasar-crud'
+
+export default {
+  name: 'Index',
+  components: { EntityIndex },
+  data () {
+    return {
+      api: '/alaa/api/v2/admin/coupon',
+      tableKeys: {
+        data: 'data',
+        total: 'meta.total',
+        currentPage: 'meta.current_page',
+        perPage: 'meta.per_page',
+        pageKey: 'couponPage'
+      },
+      inputs: [
+        {
+          type: 'Select',
+          name: 'type',
+          responseKey: '',
+          label: ' نام کالایی که از خرید آن بن دریافت کرده است',
+          col: 'col-md-6',
+          options: [],
+          optionValue: '',
+          optionLabel: ''
+        },
+        {
+          type: 'Select',
+          name: 'couponStatus',
+          label: ' وضعیت بن',
+          col: 'col-md-6',
+          options: [
+            { id: '', value: 'فعال' },
+            { id: '', value: 'باطل شده' },
+            { id: '', value: 'استفاده شده' }
+          ],
+          optionValue: 'id',
+          optionLabel: 'value'
+        }],
+      table: [],
+      createRouteName: 'Admin.Coupon.Create'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Admin/User/Show.vue
+++ src/pages/Admin/User/Show.vue
@@ -8,103 +8,36 @@
     :edit-route-name="editRouteName"
     :index-route-name="indexRouteName"
   />
-  <portlet ref="portlet">
-    <template #title>
-      روزهای آزاد کاربر
-    </template>
-    <template #toolbar>
-      <q-btn color="primary" @click="setPotentialDates">
-        ذخیره روزهای آزاد
-      </q-btn>
-    </template>
-    <template #content>
-      <div class="row">
-        <div class="col-md-4">
-          <q-date v-model="potentialDates" calendar="persian" :range="false" :multiple="true">
-            <div class="row items-center justify-end">
-              <q-btn v-close-popup label="بستن" color="primary" flat />
-            </div>
-          </q-date>
-        </div>
-        <div class="col-md-8">
-          <div class="row">
-            <div v-for="item in potentialDates" :key="item" class="col-md-2">
-              <q-card class="text-center">{{ item }}</q-card>
-            </div>
-          </div>
-        </div>
-      </div>
-      <q-inner-loading :showing="potentialDatesLoading">
-        <q-spinner-ball color="primary" size="50px" />
-      </q-inner-loading>
-    </template>
-  </portlet>
 </template>
 
 <script>
-import moment from 'moment-jalaali'
-import Portlet from 'components/Portlet'
 import EntityShow from 'components/Entity/Show/EntityShow'
-import API_ADDRESS from 'src/api/Addresses'
-import { ref } from 'vue'
-
 export default {
   name: 'Show',
-  components: { EntityShow, Portlet },
+  components: { EntityShow },
   data () {
     return {
-      potentialDates: ref([]),
       expanded: true,
-      potentialDatesLoading: false,
-      // api: API_ADDRESS.user.base,
+      api: '/alaa/api/v2/admin/user/',
       entityIdKey: 'id',
       entityParamKey: 'id',
       editRouteName: 'Admin.User.Edit',
       indexRouteName: 'Admin.User.Index',
       inputs: [
-        { type: 'input', name: 'id', responseKey: 'id', value: 'null', label: 'شناسه', col: 'col-md-3' },
-        { type: 'input', name: 'nickname', responseKey: 'nickname', value: 'null', label: 'نام', col: 'col-md-3' },
-        { type: 'input', name: 'mobile_number', responseKey: 'mobile_number', value: 'null', label: 'موبایل', col: 'col-md-3' },
-        { type: 'input', name: 'phone_number', responseKey: 'phone_number', value: 'null', label: 'تلفن', col: 'col-md-3' },
-        { type: 'input', name: 'national_code', responseKey: 'national_code', value: 'null', label: 'کدملی', col: 'col-md-3' },
-        { type: 'select', name: 'roles', responseKey: 'roles', multiple: true, optionValue: 'id', optionLabel: 'name', options: [{ name: 'ادمین کل', id: 1 }, { name: 'مبلغ', id: 3 }, { name: 'بانی', id: 4 }], col: 'col-md-3' },
-        { type: 'other', name: 'potential_dates', responseKey: 'potential_dates', value: [], col: 'col-md-3', disable: true }
+        { type: 'avatar', name: 'photo', responseKey: 'data.photo', value: null, size: '150px', col: 'col-md-12' },
+        { type: 'input', name: 'id', responseKey: 'data.id', value: 'null', label: 'شناسه', col: 'col-md-3' },
+        { type: 'input', name: 'first_name', responseKey: 'data.first_name', value: 'null', label: 'نام', col: 'col-md-3' },
+        { type: 'input', name: 'last_name', responseKey: 'data.last_name', value: 'null', label: 'نام خانوادگی', col: 'col-md-3' },
+        { type: 'input', name: 'national_code', responseKey: 'data.national_code', value: 'null', label: 'کد ملی', col: 'col-md-3' },
+        { type: 'input', name: 'email', responseKey: 'data.email', value: 'null', label: 'ایمیل', col: 'col-md-3' },
+        { type: 'input', name: 'mobile', responseKey: 'data.mobile', value: 'null', label: 'شماره همراه', col: 'col-md-3' },
+        { type: 'input', name: 'address', responseKey: 'data.address', value: 'null', label: 'آدرس', col: 'col-md-3' },
+        { type: 'input', name: 'postal_code', responseKey: 'data.postal_code', value: 'null', label: 'کد پستی', col: 'col-md-3' }
       ]
-    }
-  },
-  computed: {
-    inputPotentialDates () {
-      return this.inputs.find(item => item.name === 'potential_dates').value
-    },
-    userId () {
-      return this.inputs.find(item => item.name === 'id').value
-    }
-  },
-  watch: {
-    inputPotentialDates (newValue) {
-      this.loadPotentialDates(newValue)
     }
   },
   created () {
     this.api += '/' + this.$route.params.id
-  },
-  methods: {
-    loadPotentialDates (miladiPotentialDates) {
-      this.potentialDates = miladiPotentialDates.map(item => moment(item, 'YYYY-M-D').format('jYYYY/jMM/jDD'))
-    },
-    async setPotentialDates () {
-      const dates = this.potentialDates.map(item => moment(item, 'jYYYY/jM/jD').format('YYYY-MM-DD'))
-      this.potentialDatesLoading = true
-      this.$axios.post(API_ADDRESS.user.setPotentialDates, { user_id: this.userId, dates: dates })
-        .then((response) => {
-          this.potentialDatesLoading = false
-          this.loadPotentialDates(response.data.potential_dates)
-        })
-        .catch(() => {
-          this.potentialDatesLoading = false
-          this.potentialDates = []
-        })
-    }
   }
 }
 </script>
