c973c8e63e22db3e142d068ba02558a50616e9be
JasGH <yasaman.gonehi@gmail.com>
2022-04-27 11:56:13 +0430

    EntityCrud slots, router method added

--- src/pages/Admin/User.vue
+++ src/pages/Admin/User.vue
@@ -6,7 +6,38 @@
     v-model:create-inputs="createInputs"
     v-model:default-inputs="defaultInputs"
     v-bind="allProps"
-  />
+  >
+    <template v-slot:entity-crud-table-cell="{inputData, showConfirmRemoveDialog}">
+      <q-td :props="inputData.props">
+        <template v-if="inputData.props.col.name === 'photo'">
+          <q-avatar>
+            <q-img
+              :src="inputData.props.value"
+              placeholder-src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWBAMAAADOL2zRAAAAG1BMVEXMzMyWlpaqqqq3t7fFxcW+vr6xsbGjo6OcnJyLKnDGAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABAElEQVRoge3SMW+DMBiE4YsxJqMJtHOTITPeOsLQnaodGImEUMZEkZhRUqn92f0MaTubtfeMh/QGHANEREREREREREREtIJJ0xbH299kp8l8FaGtLdTQ19HjofxZlJ0m1+eBKZcikd9PWtXC5DoDotRO04B9YOvFIXmXLy2jEbiqE6Df7DTleA5socLqvEFVxtJyrpZFWz/pHM2CVte0lS8g2eDe6prOyqPglhzROL+Xye4tmT4WvRcQ2/m81p+/rdguOi8Hc5L/8Qk4vhZzy08DduGt9eVQyP2qoTM1zi0/uf4hvBWf5c77e69Gf798y08L7j0RERERERERERH9P99ZpSVRivB/rgAAAABJRU5ErkJggg=="
+              spinner-color="white"
+              style="height: 50px; max-width: 50px"
+            />
+          </q-avatar>
+        </template>
+        <template v-else-if="inputData.props.col.name === 'actions'">
+          <q-btn round flat dense size="md" color="info" icon="info" :to="{name:'Admin.User.Show', params: {id: inputData.props.row.id}}">
+            <q-tooltip>
+              مشاهده
+            </q-tooltip>
+          </q-btn>
+          <q-btn round flat dense size="md" color="negative" icon="delete" class="q-ml-md"
+                 @click="showConfirmRemoveDialog(inputData.props.row, 'id', getRemoveMessage(inputData.props.row))">
+            <q-tooltip>
+              حذف
+            </q-tooltip>
+          </q-btn>
+        </template>
+        <template v-else>
+          {{ inputData.props.value }}
+        </template>
+      </q-td>
+    </template>
+  </entity-crud>
 </template>
 
 <script>
@@ -41,6 +72,7 @@ export default {
           showRouteName: 'Admin.User.Show',
           editRouteName: 'Admin.User.Edit',
           indexRouteName: 'Admin.User.Index',
+          createRouteName: 'Admin.User.Create',
           entityIdKeyInResponse: 'id',
           showRouteParamKey: 'id',
           tableKeys: {
@@ -158,6 +190,14 @@ export default {
         { type: 'select', name: 'status', value: null, options: ['فعال', 'غیر فعال'], label: 'وضعیت', col: 'col-md-4' },
         { type: 'dateRange', name: 'created_at_range', value: [], label: 'بازه تاریخ عضویت', col: 'col-md-4' }
       ]
+    }
+  },
+  methods: {
+    // for index.vue
+    getRemoveMessage (row) {
+      const firstName = row.first_name
+      const lastName = row.last_name
+      return 'آیا از حذف ' + firstName + ' ' + lastName + ' اطمینان دارید؟'
     }
   },
   watch: {
