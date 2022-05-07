5393b20e7d14e710e3e53146df8e7987e4fc2220
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-05-07 12:04:41 +0430

    auth login commented

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -2,8 +2,8 @@
  * Auth middleware example.
  */
 export default function auth (/* { to, from, next, store } */ { next, store }) {
-  if (!store.getters['Auth/accessToken']) {
-    return next({ name: 'login' })
-  }
+  // if (!store.getters['Auth/accessToken']) {
+  //   return next({ name: 'login' })
+  // }
   return next()
 }
