e8d8eb48512acd830a85e18d4718b511d1f7ecb7
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-05-08 10:22:24 +0430

    base of slider see also Done

--- src/components/Widgets/Slider/Slider.vue
+++ src/components/Widgets/Slider/Slider.vue
@@ -1,10 +1,45 @@
 <template>
-
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      swipeable
+      arrows
+      height="300px"
+      class="text-purple rounded-borders"
+    >
+      <q-carousel-slide
+        v-for="(slide, index) in slides"
+        :key="index"
+        :name="slide.name"
+        :img-src="slide.imageUrl"
+      />
+    </q-carousel>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  name: 'Slider'
+  name: 'Slider',
+  props: {
+    slides: {
+      type: Array,
+      default () {
+        return [{ title: '', name: '', imageUrl: '', icon: '', tooltip: '' }]
+      }
+    }
+  },
+  created () {
+    console.log(this.slides)
+  },
+
+  setup () {
+    return {
+      slide: ref(1),
+      fullscreen: ref(false)
+    }
+  }
 }
 </script>
 
