4ff5f0d86b1a6f83c896dce2a9c1649b8f7d6ad9
JasGH <yasaman.gonehi@gmail.com>
2022-05-09 12:33:54 +0430

    Attribute Management

--- src/pages/Admin/AttributeManagement.vue
+++ src/pages/Admin/AttributeManagement.vue
@@ -1,37 +1,43 @@
 <template>
+  <!--  v-model:index-inputs="indexInputs"-->
+  <entity-index
+    v-model:value="createInputs"
+    title="اصلاح اطلاعات مقدار صفت تاریخ آزمون"
+    :api="allProps.config.api.index"
+    :table="allProps.config.table"
+    :table-keys="allProps.config.tableKeys"
+    :create-route-name="'Admin.AttributeManagement.Create'"
+  />
   <entity-crud
-    v-model:edit-inputs="editInputs"
-    v-model:index-inputs="indexInputs"
-    v-model:show-inputs="showInputs"
-    v-model:create-inputs="createInputs"
     v-model:default-inputs="defaultInputs"
-    :before-get-edit-data="testMethod1"
-    :before-load-edit-input-data="testMethod2"
-    :after-load-edit-input-data="testMethod3"
+    v-model:create-inputs="createInputs"
     v-bind="allProps"
   >
     <template v-slot:before-entity-create>
-      <q-select
-        v-model="model"
-        use-input
-        use-chips
-        multiple
-        input-debounce="0"
-        @new-value="createValue"
-        :options="tags"
+      <entity-index
+        v-model:value="createInputs"
+        title="اصلاح اطلاعات مقدار صفت تاریخ آزمون"
+        :api="allProps.config.api.index"
+        :table="allProps.config.table"
+        :table-keys="allProps.config.tableKeys"
+        :create-route-name="'Admin.AttributeManagement.Create'"
       />
+<!--      <q-select-->
+<!--        v-model="model"-->
+<!--        use-input-->
+<!--        use-chips-->
+<!--        multiple-->
+<!--        input-debounce="0"-->
+<!--        @new-value="createValue"-->
+<!--        :options="tags"-->
+<!--      />-->
     </template>
     <template v-slot:entity-crud-table-cell="{inputData, showConfirmRemoveDialog}">
       <q-td :props="inputData.props">
         <template v-if="inputData.props.col.name === 'actions'">
-          <q-btn round flat dense size="md" color="info" icon="info" :to="{name:'Admin.Content.Show', params: {id: inputData.props.row.id}}">
+          <q-btn round flat dense size="md" color="info" icon="info" :to="{name:'Admin.AttributeManagement.Edit', params: {id: inputData.props.row.id}}">
             <q-tooltip>
-              مشاهده
-            </q-tooltip>
-          </q-btn>
-          <q-btn round flat dense size="md" color="info" icon="info" :to="{name:'Admin.Content.Edit', params: {id: inputData.props.row.id}}">
-            <q-tooltip>
-              ویرایش
+              اصلاح
             </q-tooltip>
           </q-btn>
           <q-btn round flat dense size="md" color="negative" icon="delete" class="q-ml-md"
@@ -68,6 +74,7 @@ export default {
       expanded: true,
       allProps: {
         config: {
+          // toDo : temp, 'Content' should be replaced with 'order'
           api: {
             show: API_ADDRESS.attributeManagement.show.base,
             edit: API_ADDRESS.attributeManagement.edit.base,
@@ -75,21 +82,22 @@ export default {
             index: API_ADDRESS.attributeManagement.index.base
           },
           title: {
-            show: 'اطلاعات محتوا',
-            edit: 'اطلاعات محتوا',
-            create: 'ثبت محتوای جدید',
-            index: 'لیست محتوا'
+            show: 'اطلاعات صفت',
+            edit: 'ویرایش صفت',
+            create: 'افزودن صفت جدید',
+            index: 'لیست صفت ها'
           },
-          showRouteName: 'Admin.Content.Show',
-          editRouteName: 'Admin.Content.Edit',
-          indexRouteName: 'Admin.Content.Index',
-          createRouteName: 'Admin.Content.Create',
+          // toDo : temp, 'Content' should be replaced with 'order'
+          showRouteName: 'Admin.AttributeManagement.Show',
+          editRouteName: 'Admin.AttributeManagement.Edit',
+          indexRouteName: 'Admin.AttributeManagement.Index',
+          createRouteName: 'Admin.AttributeManagement.Create',
           tableKeys: {
             data: 'data',
             total: 'meta.total',
             currentPage: 'meta.current_page',
             perPage: 'meta.per_page',
-            pageKey: 'contentPage'
+            pageKey: 'productPage'
           },
           table: {
             columns: [
@@ -101,44 +109,51 @@ export default {
                 field: row => row.id
               },
               {
-                name: 'name',
+                name: 'first_name',
                 required: true,
-                label: 'عنوان',
+                label: 'نام (اصلی)',
                 align: 'left',
-                field: row => row.name
+                field: row => row.first_name
               },
               {
-                name: 'type',
+                name: 'first_name',
                 required: true,
-                label: 'نوع',
+                label: 'نام قابل نمایش (فارسی)',
                 align: 'left',
-                field: row => (row.content_type.name === 'video') ? 'ویدئو' : 'جزوه'
+                field: row => row.last_name
               },
               {
-                name: 'enable',
+                name: 'mobile',
                 required: true,
-                label: 'فعال',
+                label: 'نوع کنترل صفت',
                 align: 'left',
-                field: row => (row.enable) ? 'فعال' : 'غیرفعال'
+                field: row => row.mobile
               },
               {
-                name: 'is_free',
+                name: 'national_code',
                 required: true,
-                label: 'قیمت گذاری',
+                label: 'توضیح',
                 align: 'left',
-                field: row => (row.is_free) ? 'رایگان' : 'پولی'
+                field: row => row.national_code
               },
               {
-                name: 'description',
+                name: 'price',
                 required: true,
-                label: 'توضیحات',
+                label: 'زمان درج',
                 align: 'left',
-                field: row => row.description
+                field: row => row.price
+              },
+              {
+                name: 'paid_price',
+                required: true,
+                label: 'زمان اصلاح',
+                align: 'left',
+                field: row => row.price
               },
               {
                 name: 'actions',
                 required: true,
-                label: 'عملیات',
+                label: '',
                 align: 'left',
                 field: ''
               }
@@ -148,37 +163,29 @@ export default {
         }
       },
       defaultInputs: [
-        { type: 'file', name: 'photo', responseKey: 'data.photo', size: '250px', col: 'col-md-3' },
-        { type: 'space', col: 'col-md-12' },
-        { type: 'optionGroupRadio', name: 'is_free', responseKey: 'data.is_free', label: '', col: 'col-md-3', options: [{ label: 'رایگان باشد', value: 1 }, { label: 'رایگان نباشد', value: 0 }] },
-        { type: 'optionGroupRadio', name: 'enable', options: [{ label: 'غیرفعال', value: 0 }, { label: 'فعال', value: 1 }], responseKey: 'data.enable', label: 'وضعیت', col: 'col-md-3' },
-        { type: 'optionGroupRadio', name: 'discount', options: [{ label: 'عدم نمایش', value: 0 }, { label: 'نمایش', value: 1 }], responseKey: 'data.discount', label: 'نمایش', col: 'col-md-3' },
-        { type: 'optionGroupCheckbox', multiple: true, options: [{ label: 'درج کیفیت hq', value: 0 }, { label: 'درج کیفیت HD', value: 1 }, { label: 'درج کیفیت 240p', value: 2 }], name: 'enable', value: [1], label: 'کیفیت محتوا', col: 'col-md-3' },
-        { type: 'file', name: 'video-1', label: 'کیفیت عالی', responseKey: 'data.file.video[0].link', size: '250px', col: 'col-md-4' },
-        { type: 'file', name: 'video-2', responseKey: 'data.file.video[0].link', label: 'کیفیت خوب', size: '250px', col: 'col-md-4' },
-        { type: 'file', name: 'video-3', responseKey: 'data.file.video[0].link', label: 'کیفیت متوسط', size: '250px', col: 'col-md-4' },
-        { type: 'input', name: 'id', responseKey: 'data.id', label: 'نمایان شدن برای کاربران', col: 'col-md-3' },
-        { type: 'input', name: 'id', responseKey: 'data.id', label: 'تاریخ درج', col: 'col-md-3' },
-        { type: 'input', name: 'order', responseKey: 'data.order', label: 'آیدی دبیر', col: 'col-md-3' },
-        { type: 'input', name: 'redirect_url', responseKey: 'data.redirect_url', label: 'آدرس ریدایرکت', col: 'col-md-3' },
-        { type: 'select', name: 'redirect_code', responseKey: 'data.product_type.display_name', options: [{ label: '301 (دائمی)', value: 301 }, { label: '302 (موقتی)', value: 302 }], label: 'کد ریدایرکت', col: 'col-md-3' },
-        { type: 'input', name: 'order', responseKey: 'data.order', label: 'ترتیب', col: 'col-md-3' },
-        { type: 'select', name: 'content_type_id', value: null, options: [{ label: 'ویدئو', value: 0 }, { label: 'جزوه', value: 8 }, { label: 'مقاله', value: 3 }], label: 'نوع محتوا', col: 'col-md-3' },
-        { type: 'select', name: 'attribute_set', responseKey: 'data.attribute_set.id', label: 'سکشن محتوا', col: 'col-md-3', options: [{ label: 'اردو', value: 1 }, { label: 'همایش', value: 2 }, { label: 'فیلم استودیو', value: 3 }, { label: 'جزوه درس', value: 4 }, { label: 'کتاب', value: 5 }, { label: 'پیش فرض', value: 6 }, { label: 'محصول اشتراک', value: 7 }, { label: 'آزمون', value: 8 }] },
-        { type: 'input', name: 'title', responseKey: 'data.title', label: 'نام*', col: 'col-md-3' },
-        { type: 'input-editor', name: 'short_description', responseKey: 'data.description.short', value: 'مینبتلمینبلتیمبلنتیلیبیبلیب', label: 'توضیحات', col: 'col-md-12' },
-        { type: 'select', name: 'tags', options: [], responseKey: 'data.tags', multiple: true, useChips: true, createNewValue: true, label: 'تگ ها', col: 'col-md-6' },
-        { type: 'input', name: 'tags-title', responseKey: 'data.tags.title', label: 'متن تگ ها', col: 'col-md-6' },
-        { type: 'input', name: 'order', responseKey: '', label: 'ابر عنوان', col: 'col-md-6' },
-        { type: 'input', name: 'order', responseKey: '', label: 'ابر توضیح', col: 'col-md-6' }
+        { type: 'input', name: 'name', value: null, label: 'نام صفت', col: 'col-md-3' },
+        { type: 'input', name: 'name', value: null, label: 'نام قابل نمایش', col: 'col-md-3' },
+        { type: 'select', name: 'content_type_id', label: 'نوع کنترل صفت', col: 'col-md-3', value: null, options: [{ label: 'select', value: 0 }, { label: 'groupedCheckbox', value: 8 }, { label: 'switch', value: 3 }] },
+        { type: 'select', name: 'content_type_id', label: 'نوع صفت', col: 'col-md-3', value: null, options: [{ label: 'صفت اصلی', value: 0 }, { label: 'صفت غیر اصلی', value: 8 }, { label: 'صفت توضیحی', value: 3 }, { label: 'اشتراک', value: 3 }] },
+        { type: 'input', name: 'name', value: null, label: 'توضیح درباره صفت (اختیاری)', col: 'col-md-12' },
+        { type: 'space', col: 'col-md-12' }
       ],
-      createInputs: [],
+      createInputs: [
+        { type: 'input', name: 'name', value: null, label: 'نام صفت', col: 'col-md-3' },
+        { type: 'input', name: 'name', value: null, label: 'نام قابل نمایش', col: 'col-md-3' },
+        { type: 'select', name: 'content_type_id', label: 'نوع کنترل صفت', col: 'col-md-3', value: null, options: [{ label: 'select', value: 0 }, { label: 'groupedCheckbox', value: 8 }, { label: 'switch', value: 3 }] },
+        { type: 'select', name: 'content_type_id', label: 'نوع صفت', col: 'col-md-3', value: null, options: [{ label: 'صفت اصلی', value: 0 }, { label: 'صفت غیر اصلی', value: 8 }, { label: 'صفت توضیحی', value: 3 }, { label: 'اشتراک', value: 3 }] },
+        { type: 'input', name: 'name', value: null, label: 'توضیح درباره صفت (اختیاری)', col: 'col-md-12' }
+      ],
       editInputs: [],
       showInputs: [],
       indexInputs: [
-        { type: 'input', name: 'id', value: null, label: 'عنوان', col: 'col-md-3' },
-        { type: 'input', name: 'name', value: null, label: 'نام', col: 'col-md-3' },
-        { type: 'input', name: 'description', value: null, label: 'توضیحات', col: 'col-md-3' },
+        { type: 'input', name: 'id', value: null, label: 'نام (اصلی)', col: 'col-md-3' },
+        { type: 'input', name: 'name', value: null, label: 'نام قابل نمایش (فارسی)', col: 'col-md-3' },
+        { type: 'input', name: 'description', value: null, label: 'نوع کنترل صفت', col: 'col-md-3' },
+        { type: 'input', name: 'description', value: null, label: 'توضیح', col: 'col-md-3' },
+        { type: 'input', name: 'description', value: null, label: 'زمان درج', col: 'col-md-3' },
+        { type: 'input', name: 'description', value: null, label: 'زمان اصلاح', col: 'col-md-3' },
         // todo : مقاله exact value unknown
         { type: 'select', name: 'content_type_id', value: null, options: [{ label: 'ویدئو', value: 0 }, { label: 'جزوه', value: 8 }, { label: 'مقاله', value: 3 }], label: 'نوع محتوا', col: 'col-md-3' },
         { type: 'select', name: 'is_free', value: null, options: [{ label: 'رایگان', value: 1 }, { label: 'غیر رایگان', value: 0 }], label: 'وضعیت رایگان / غیر رایگان', col: 'col-md-3' },
@@ -216,11 +223,7 @@ export default {
         }
         done(val, 'toggle')
       }
-    },
-    testMethod () {},
-    testMethod1 () {},
-    testMethod2 () {},
-    testMethod3 () {}
+    }
   },
   watch: {
     // editInputs: {
