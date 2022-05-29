00763d698ad3b7d7c6a0291ad7ed207545a63384
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-05-29 13:36:43 +0430

    banner model done

--- src/models/Banner.js
+++ src/models/Banner.js
@@ -6,7 +6,36 @@ class Banner extends Model {
       { key: 'id' },
       { key: 'title' },
       { key: 'body' },
-      { key: 'photo' },
+      {
+        key: 'features',
+        default: {
+          xl: {
+            src: '',
+            width: '',
+            height: ''
+          },
+          lg: {
+            src: '',
+            width: '',
+            height: ''
+          },
+          md: {
+            src: '',
+            width: '',
+            height: ''
+          },
+          sm: {
+            src: '',
+            width: '',
+            height: ''
+          },
+          xs: {
+            src: '',
+            width: '',
+            height: ''
+          }
+        }
+      },
       { key: 'link' },
       { key: 'order' },
       { key: 'ratio' }
