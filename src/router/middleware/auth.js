7f258f63e6f545a426fddf474835a660db2a70ef
kerasus <aliesmaeeli20@gmail.com>
2022-05-30 12:35:07 +0430

    update routes

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -1,10 +1,6 @@
-/**
- * Auth middleware example.
- */
 export default function auth (/* { to, from, next, store } */ { next, store }) {
+  if (!store.getters['Auth/accessToken']) {
+    return next({ name: 'login' })
+  }
   return next()
-  // if (!store.getters['Auth/accessToken']) {
-  //   return next({ name: 'login' })
-  // }
-  // return next()
 }
