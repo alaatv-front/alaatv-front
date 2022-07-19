f1c32250a5f0c68cd2a8c096a438adf254701f5f
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-19 10:08:30 +0430

    product show

--- src/pages/User/Product/Show.vue
+++ src/pages/User/Product/Show.vue
@@ -1,10 +1,74 @@
 <template>
-
+<page-builder :sections="sections"/>
 </template>
 
 <script>
+import PageBuilder from 'components/PageBuilder/PageBuilder'
+import { BlockList } from 'src/models/Block'
+import API_ADDRESS from 'src/api/Addresses'
+
 export default {
-name: "Show"
+  name: 'Show',
+  components: { PageBuilder },
+  data () {
+    return {
+      sections: [
+        {
+          data: {
+            rows: [
+              {
+                cols: [
+                  {
+                    widgets: [
+                      {
+                        name: 'productIntroduction',
+                        data:{}
+                      }
+                    ]
+                  },
+                ],
+                options: {
+                  boxed: true
+                }
+              },
+              {
+                cols: [
+                  {
+                    widgets: [
+                      {
+                        name: 'productDemos',
+                        data:{}
+                      }
+                    ]
+                  },
+                ],
+                options: {
+                  boxed: true
+                }
+              },
+              {
+                cols: [
+                  {
+                    widgets: [
+                      {
+                        name: 'productReview',
+                        data:{}
+                      }
+                    ]
+                  },
+                ],
+                options: {
+                  boxed: true
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  created () {},
+  methods: {}
 }
 </script>
 
