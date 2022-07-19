ec978993f9227bec311af90a8ecb54e252e893d1
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-19 18:20:23 +0430

    products widgets

--- src/components/Widgets/Product/productIntroduction.vue
+++ src/components/Widgets/Product/productIntroduction.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="intro-features">
-    <div class="title"></div>
-    <div class="features-tag"></div>
-    <div class="price"></div>
+  <div class="product-introduction row">
+    <div class="intro-features col-6">
+      <div class="title">
+        ویژگی های این محصول
+      </div>
+
+      <div class="product-info-box">
+        <div
+          v-for="(info, index) in information"
+          :key="index"
+          class="product-info"
+        >
+          <div class="info-header">
+            <q-img :src="info.src"/>
+            <p class="info-title">
+              {{info.title}}
+            </p>
+          </div>
+
+          <div class="info-content">
+            <p class="info-value">
+              {{info.value}}
+            </p>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="product-price">
+        <span class="discount"></span>
+        <span class="discount"></span>
+        <q-btn class="discount"></q-btn>
+      </div>
+    </div>
+    <div class="intro-video col-6">
+      <video-player/>
+    </div>
   </div>
-  <div class="intro-video"></div>
 </template>
 
 <script>
+import VideoPlayer from 'src/components/VideoPlayer.vue'
+
 export default {
-  name: 'productIntroduction'
+  name: 'productIntroduction',
+  components: { VideoPlayer },
+  data () {
+    return {
+      information: [
+        {
+          src: '',
+          title: 'مدرس',
+          value: ''
+        },
+        {
+          src: '',
+          title: '',
+          value: ''
+        },
+        {
+          src: '',
+          title: '',
+          value: ''
+        },
+
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.product-introduction {
+  .intro-features {
+    .features {
+      .feature {
 
+      }
+    }
+  }
+  .intro-video {
+
+  }
+}
 </style>
--- src/components/Widgets/Product/productReview.vue
+++ src/components/Widgets/Product/productReview.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>productReview</div>
+  <div class="product-review">
+    <p class="review-title"> بررسی محصول</p>
+    <q-card class="custom-card">
+      <q-card-section v-html="">
+
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
+
 export default {
-name: "productReview"
+name: "productReview",
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    }
+  },
+  data() {
+
+  }
 }
 </script>
 
