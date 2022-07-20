303906ce27c23f09b2953959e1cfa0e0070c3e67
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-20 11:56:46 +0430

    created product show widget

---
+++ src/components/Widgets/Product/show.vue
@@ -0,0 +1,27 @@
+<template>
+  <product-introduction />
+  <product-demos />
+  <product-review />
+</template>
+
+<script>
+import productIntroduction from 'components/Widgets/Product/productIntroduction'
+import productDemos from 'components/Widgets/Product/productDemos'
+import productReview from 'components/Widgets/Product/productReview'
+import { mixinWidget } from 'src/mixin/Mixins'
+
+export default {
+  name: 'show',
+  components: { productIntroduction, productDemos, productReview },
+  mixins:[mixinWidget],
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
