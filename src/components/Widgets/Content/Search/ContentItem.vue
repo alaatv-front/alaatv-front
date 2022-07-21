e93989878e7163ab5e0d9a752304dbc264f30ee9
mitraKhm <mitra.kh1995@gmail.com>
2022-07-21 14:39:57 +0430

    content item style stable

--- src/components/Widgets/Content/Search/ContentItem.vue
+++ src/components/Widgets/Content/Search/ContentItem.vue
@@ -17,7 +17,7 @@
       :href="'https://alaatv.com/c/'+ data.id"
       class="m-link content-link"
     >
-      <div class="flex items-center">
+      <div class="content-order flex items-center">
         <div class="videoOrder-title text-primary q-mr-sm">جلسه</div>
         <q-avatar color="primary"
                   size="lg"
@@ -25,38 +25,39 @@
           <div class="videoOrder-number">{{ data.order }}</div>
         </q-avatar>
       </div>
-
-      <div class="content">
-        <div class="title">
+      <div class="inner-content q-pl-md ">
+        <div class="title q-mt-md">
           <p>
             {{ data.set ? data.set.short_title : '' }}
           </p>
         </div>
         <div class="details">
           <div class="videoDetaileWrapper">
-            <span>
-              <svg
-                viewBox="0 0 24 24"
-                preserveAspectRatio="xMidYMid meet"
-                focusable="false"
-                class="style-scope yt-icon"
-              >
-                <path
-                  d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z"
-                  class="style-scope yt-icon">
-                </path>
-              </svg>
-            </span>
-
-            <span>
-              از دوره ی
-            </span>
-            <span class="content-description">
-              {{ data.set ? data.set.title : '' }}
-            </span>
-            <br>
-            <i
-              class="fa fa-calendar-alt m--margin-right-5"
+            <div class="flex items-center">
+              <span class="svg-icon flex items-center">
+                <svg
+                  viewBox="0 0 24 24"
+                  preserveAspectRatio="xMidYMid meet"
+                  focusable="false"
+                  class="style-scope yt-icon"
+                >
+                  <path
+                    d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z"
+                    class="style-scope yt-icon">
+                  </path>
+                </svg>
+              </span>
+              <p class="content-description">
+                از دوره ی
+              </p>
+              <p class="content-description">
+                {{ data.set ? data.set.title : '' }}
+              </p>
+            </div>
+            <q-icon
+              name="mdi-calendar-text"
+              class="q-mr-sm"
+              color="black"
               data-toggle="m-tooltip"
               data-placement="top"
               data-original-title="تاریخ بروزرسانی"
@@ -64,14 +65,9 @@
             <span class="content-description">
               {{ data.updated_at }}
             </span>
-            <div class="videoOrder">
-              <div class="videoOrder-title">جلسه</div>
-              <div class="videoOrder-number">{{ data.order }}</div>
-              <div class="videoOrder-om"> اُم</div>
-            </div>
           </div>
           <div class="videoDescription content-description">
-            **{{ data.title }}
+            {{ data.title }}
           </div>
         </div>
       </div>
@@ -98,6 +94,9 @@ export default {
 
 <style lang="scss" scoped>
 //#212529
+p {
+  margin-bottom: 0;
+}
 
 .content-box {
   border-radius: 15px;
@@ -131,25 +130,53 @@ export default {
   }
 
   .content-link {
+    position: relative;
     width: 100%;
+    .content-order{
+      position: absolute;
+      right: 15px;
+      height: 100%;
+    }
 
-    .content {
+    .inner-content {
       width: 100%;
 
       .title {
-
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 31px;
+        letter-spacing: -0.03em;
+        color: #3D3F46;
       }
 
       .details {
-        .videoDetaileWrapper{
-          .style-scope{
-            width: 36px;
+        .videoDetaileWrapper {
+          .svg-icon {
+            .style-scope {
+              width: 20px;
+            }
+          }
+
+          .content-description {
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 31px;
+            letter-spacing: -0.03em;
+            color: #3D3F46;
+          }
+
+          .mdi-calendar-text {
+            width: 20px;
+            font-size: 18px;
           }
         }
 
         .content-description {
-          font-size: 1.2rem;
-          font-weight: 300;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 31px;
+          letter-spacing: -0.03em;
+          color: #3D3F46;
           @media screen and (max-width: 500px) {
             font-size: 12px !important;
           }
@@ -158,11 +185,13 @@ export default {
 
     }
   }
-  &:hover .content-hover{
+
+  &:hover .content-hover {
     background-color: #ff8e0017;
     border-radius: 15px;
     width: 100% !important;
   }
+
   .content-hover {
     position: absolute;
     top: 0;
