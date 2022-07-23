4cfc9f385aebe1898d9f5b48344f29f5577cece0
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-23 10:59:49 +0430

    productReview fixed

--- src/components/Widgets/Product/productReview.vue
+++ src/components/Widgets/Product/productReview.vue
@@ -2,24 +2,45 @@
   <div class="product-review">
     <p class="review-title"> بررسی محصول</p>
     <q-card class="custom-card">
-      <q-card-section v-html="">
+      <q-card-section v-html="description.long"/>
 
-      </q-card-section>
     </q-card>
   </div>
 </template>
 
 <script>
+import { mixinWidget } from 'src/mixin/Mixins'
+import { QuasarTemplateBuilderAppLayout } from 'quasar-template-builder'
+import * as getters from 'src/store/AppLayout/getters'
 
 export default {
 name: "productReview",
+  mixins:[mixinWidget],
+
   props: {
     data: {
       type: Object,
       default: {}
     }
   },
-  data() {
+  data () {
+    return {
+      description: {
+        long: '',
+        short: '',
+        slogan: '',
+      }
+    }
+  },
+  watch: {
+    data: {
+      deep: true,
+      handler (newValue) {
+        Object.assign(this.description, newValue)
+      }
+    }
+  },
+  methods: {
 
   }
 }
