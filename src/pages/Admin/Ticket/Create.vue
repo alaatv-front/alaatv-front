56a6eb47184e896b5e1a1251e980146ce15fb4b4
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-07-23 18:35:21 +0430

    ticket create

--- src/pages/Admin/Ticket/Create.vue
+++ src/pages/Admin/Ticket/Create.vue
@@ -2,11 +2,37 @@
   <entity-create
     v-model:value="inputs"
     title="ثبت محصول جدید"
-    :api="api"
+    :api="api1"
     :entity-id-key-in-response="entityIdKeyInResponse"
     :show-route-param-key="showRouteParamKey"
     :index-route-name="indexRouteName"
     :show-route-name="showRouteName"
+  >
+    <template #before-form-builder>
+      <q-banner v-if="beforeFormBuilder"
+                inline-actions
+                rounded
+                class="bg-orange text-white q-ma-md">
+        before form builder
+        <template v-slot:action>
+          <q-btn flat
+                 label="Dismiss"
+                 @click="beforeFormBuilder = false" />
+        </template>
+      </q-banner>
+    </template>
+  </entity-create>
+  <q-separator class="q-my-md" />
+  <entity-attachment
+    v-model:value="crudFormInputs"
+    expanded-default
+    :title="'entity attachment titile'"
+    :action-title="'attach action title'"
+    :list-title="'entity attached list'"
+    :list-show-route-name="'Admin.BlockManagement.Show'"
+    :attachment-list-api-address="api"
+    :table="table"
+    :table-keys="tableKeys"
   />
 </template>
 
@@ -20,15 +46,129 @@ export default {
   data () {
     return {
       expanded: true,
-      api: API_ADDRESS.ticket.create.base,
+      api1: API_ADDRESS.ticket.create.base,
+      api2: API_ADDRESS.ticket.create.base,
+      api3: API_ADDRESS.ticket.create.base,
       entityIdKeyInResponse: 'id',
       showRouteParamKey: 'id',
       showRouteName: 'Admin.Ticket.Show',
       indexRouteName: 'Admin.Ticket.Index',
       inputs: [
-        { type: 'input', name: 'name', responseKey: 'data.name', value: '', label: 'نام', col: 'col-md-6' },
-        { type: 'input', name: 'job', responseKey: 'data.job', value: '', label: 'شغل', col: 'col-md-6' }
-      ]
+        { type: 'input', name: 'title', responseKey: 'data.title', value: '', label: 'عنوان', col: 'col-md-6' },
+        {
+          type: 'formBuilder',
+          name: 'formBuilderCol',
+          col: 'col-md-6',
+          value: [
+            { type: 'separator', label: 'اولویت:', vertical: 'false', col: 'col-md-1' },
+            {
+              type: 'toggleButton',
+              name: 'priority',
+              responseKey: 'data.priority',
+              options: [
+                { label: 'کم', value: '1' },
+                { label: 'متوسط', value: '2' },
+                { label: 'فوری', value: '3' },
+                { label: 'بحرانی', value: '4' }
+              ],
+              col: 'col-md-4',
+              textColor: 'black',
+              size: '20px'
+            }
+
+          ]
+
+        }
+      ],
+      crudFormInputs: [
+        {
+          type: 'EntityAttachment',
+          name: 'formBuilderCol',
+          col: 'col-md-6',
+          value: [
+            {
+              type: 'entity',
+              name: 'ChangeUser',
+              label: 'تغییر کاربر',
+              selectionMode: 'single',
+              buttonColor: 'indigo',
+              buttonTextColor: 'black',
+              buttonBadgeColor: 'orange',
+              indexConfig: {
+                apiAddress: 'https://reqres.in/api/users',
+                tableTitle: 'لیست محصولات',
+                tableKeys: {
+                  data: 'data',
+                  total: 'total',
+                  currentPage: 'page',
+                  perPage: 'per_page',
+                  pageKey: 'page'
+                },
+                table: {
+                  columns: [
+                    {
+                      name: 'id',
+                      required: true,
+                      label: '#',
+                      align: 'left',
+                      field: row => row.id
+                    },
+                    {
+                      name: 'thumbnail',
+                      required: true,
+                      label: 'تصویر',
+                      align: 'left',
+                      field: row => row.avatar
+                    },
+                    {
+                      name: 'first_name',
+                      required: true,
+                      label: 'نام',
+                      align: 'left',
+                      field: row => row.first_name
+                    },
+                    {
+                      name: 'last_name',
+                      required: true,
+                      label: 'نام خانوادگی',
+                      align: 'left',
+                      field: row => row.last_name
+                    },
+                    {
+                      name: 'email',
+                      required: true,
+                      label: 'ایمیل',
+                      align: 'left',
+                      field: row => row.email
+                    },
+                    {
+                      name: 'actions',
+                      required: true,
+                      label: '',
+                      align: 'left',
+                      field: ''
+                    }
+                  ],
+                  data: []
+                },
+                inputs: [
+                  { type: 'input', name: 'id', value: null, label: 'شناسه', col: 'col-md-3' },
+                  { type: 'input', name: 'first_name', value: null, label: 'نام', col: 'col-md-3' },
+                  { type: 'input', name: 'last_name', value: null, label: 'نام خانوادگی', col: 'col-md-3' }
+                ],
+                itemIdentifyKey: 'id',
+                itemIndicatorKey: 'first_name',
+                showTableItemsRouteName: 'Admin.BlockManagement.Show'
+              },
+              value: [],
+              responseKey: '',
+              selected: [],
+              col: 'col-md-4'
+            }
+          ]
+        }],
+      beforeFormBuilder: true,
+      afterFormBuilder: true
     }
   },
   created () {
