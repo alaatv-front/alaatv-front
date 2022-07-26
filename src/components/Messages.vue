6d77294fc81d82d9fbe2836717ad0bca9cc1adc6
Nima.mr <nima1380620@yahoo.com>
2022-07-26 18:06:26 +0430

    message component has been updated!

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,9 +1,6 @@
 <template>
-  <av-waveform
-    audio-src="https://dls.music-fa.com/tagdl/1401/Morteza%20Pashaei%20-%20Saat%20Naro%20Jolo%20(320).mp3"
-  ></av-waveform>
   <q-card style="margin-top: 50px;">
-    <div dir="ltr">
+    <div :dir="ltrOrRtl">
       <div class="user-img">
         <q-img width="85px"
                height="85px"
@@ -11,20 +8,28 @@
                :src="data.user.photo" />
       </div>
     </div>
-    <q-card-section class="message-user-info d-flex"
-                    style="justify-content: end">
+    <q-card-section class="d-flex"
+                    :class="isUserCustomer ? 'admin-info' : 'user-info'">
       <div class="d-flex"
            style="margin-top: 33px">
-        <span v-if="isUserCustumer"
-              style="margin-left: 5px;">
+        <q-img v-if="!isUserCustomer"
+               width="20px"
+               height="25px"
+               src="https://nodes.alaatv.com/upload/favicon.ico" />
+        <span v-if="isUserCustomer"
+              class="q-mr-xs">
           ماه:
         </span>
-        <span v-else>
-          {{data.user.role}}
+        <span v-else
+              class="q-mx-xs q-mt-xs">
+          {{data.user.role}}:
         </span>
-        {{data.user.first_name}}
-        {{data.user.last_name}}
-        <q-icon size="16px"
+        <span :class="!isUserCustomer ? 'q-mt-xs' : ''">
+          {{data.user.first_name}}
+          {{data.user.last_name}}
+        </span>
+        <q-icon v-if="isUserCustomer"
+                size="16px"
                 name="isax:user"
                 style="margin-right: 5px; margin-top: 2px" />
       </div>
@@ -32,32 +37,77 @@
     <q-card-section class="message-body">
       <div class="body">
         <div v-html="data.body" />
-
+        <div dir="ltr"
+             v-if="data.files.voice">
+          <div class="d-flex voice-player-section">
+            <q-btn v-if="!showVoicePlayerIsPlaying"
+                   size="30px"
+                   unelevated
+                   icon="isax:play"
+                   class="play-btn"
+                   @click="playRecordedVoice">
+            </q-btn>
+            <q-btn v-else
+                   size="30px"
+                   unelevated
+                   @click="pauseRecordedVoice">
+              <q-icon name="isax:pause"></q-icon>
+            </q-btn>
+            <av-waveform class="av-waveform"
+                         ref="waveform"
+                         :audio-src="data.files.voice"
+                         :playtime-font-family="'IRANSans'"
+                         :audio-controls="false"
+                         :canv-width="900"
+                         :canv-height="64"
+            ></av-waveform>
+          </div>
+        </div>
         <q-img v-if="data.files.photo"
                :src="data.files.photo"
                style="margin: 30px 0"
         />
       </div>
       <q-separator class="q-my-md"></q-separator>
-      <q-chip color="blue"
-              style="color: #FFFFFF; height: 30px;"
-              square>
-        {{ makeDateShamsi(data.created_at) }}
-      </q-chip>
+      <div class="d-flex">
+        <q-chip color="blue"
+                style="color: #FFFFFF; height: 30px;"
+                square>
+          {{ makeDateShamsi(data.created_at) }}
+        </q-chip>
+        <q-expansion-item
+          v-if="!isUserCustomer"
+          class="report-panel"
+          icon="isax:ticket"
+          label="گزارش پاسخ نامناسب"
+        >
+          <q-card>
+            <q-card-section>
+              <q-input type="textarea"
+                       v-model="userReportDescription"></q-input>
+            </q-card-section>
+            <q-card-actions>
+              <q-btn flat
+                     @click="sendReport"
+                     color="blue">
+                ارسال گزارش
+              </q-btn>
+            </q-card-actions>
+          </q-card>
+        </q-expansion-item>
+      </div>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
 import moment from 'moment-jalaali'
-// import AvBars from 'vue-audio-visual/src/components/AvWaveform.js'
 import AvWaveform from 'vue-audio-visual/src/components/AvWaveform'
-// import AvWaveform from 'vue-audio-visual'
 
 export default {
   name: 'Messages',
   components: {
-    'av-waveform': AvWaveform
+    AvWaveform
   },
   props: {
     data: {
@@ -65,20 +115,62 @@ export default {
       default: null
     }
   },
+  data () {
+    return {
+      showVoicePlayerIsPlaying: false,
+      audioPlayerLasrPlayedTime: 0,
+      userReportDescription: ''
+    }
+  },
   methods: {
     makeDateShamsi (date) {
       return moment(date, 'YYYY-M-D HH:mm:ss').format('jYYYY/jMM/jDD HH:mm:ss')
+    },
+    playRecordedVoice () {
+      const audioPlayer = this.$refs.waveform.$el.children[0].children[0],
+        that = this
+      audioPlayer.src = this.data.files.voice
+      audioPlayer.currentTime = this.audioPlayerLasrPlayedTime
+      audioPlayer.onended = function () {
+        // console.log('audioPlayer.onended');
+        audioPlayer.pause()
+        audioPlayer.currentTime = 0
+        that.audioPlayerLasrPlayedTime = 0
+        that.showVoicePlayerIsPlaying = false
+      }
+      audioPlayer.play()
+      this.showVoicePlayerIsPlaying = true
+    },
+    pauseRecordedVoice () {
+      const audioPlayer = this.$refs.waveform.$el.children[0].children[0]
+      audioPlayer.pause()
+      this.audioPlayerLasrPlayedTime = audioPlayer.currentTime
+      this.showVoicePlayerIsPlaying = false
+    },
+    sendReport () {
+      console.log('resport sent!')
     }
   },
   computed: {
-    isUserCustumer () {
+    isUserCustomer () {
       return this.data.user.role === 'کاربر'
+    },
+    ltrOrRtl () {
+      if (this.isUserCustomer) {
+        return 'ltr'
+      } else {
+        return 'rtl'
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.av-waveform {
+  width: calc( 100% - 76px);
+  padding-top: 9px;
+}
 .user-img {
   position: absolute;
   margin-left: 30px;
@@ -89,9 +181,31 @@ export default {
  .d-flex {
    display: flex !important;
  }
- .message-user-info {
+ .user-info {
+   justify-content: start;
+ }
+.admin-info {
+   justify-content: end;
  }
  .message-body {
    padding-top: 0;
  }
+ .report-panel {
+   width: 100%;
+   margin-left: 30px;
+ }
+</style>
+<style scoped lang="scss">
+.voice-player-section {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  .q-btn {
+    margin-bottom: 5px;
+    background: #34bfa3;
+    color: #FFF;
+    &:deep(.q-btn__content) {
+      margin: 15px;
+    }
+  }
+}
 </style>
