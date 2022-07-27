ffbb4d2bf882afb5720cff80d9d912aa132e4c73
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-27 09:58:40 +0430

    added isUserLogin in auth getters

--- src/store/Auth/getters.js
+++ src/store/Auth/getters.js
@@ -2,10 +2,17 @@ import { User } from 'src/models/User'
 
 export function user (state) {
   return new User(state.user)
+
 }
+
+export function isUserLogin (state) {
+  return state.user.id
+}
+
 export function accessToken (state) {
   return state.accessToken
 }
+
 export function redirectTo (state) {
   return state.redirectTo
 }
