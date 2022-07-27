aefa437e8c550b6c8a0bff4eb56a9beab54c24a8
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-07-27 09:55:50 +0430

    cart store fixed

--- src/store/Cart/getters.js
+++ src/store/Cart/getters.js
@@ -1,10 +1,5 @@
 import { Cart } from 'src/models/Cart'
-import { CartItemList } from 'src/models/CartItem'
 
 export function cart(state) {
   return new Cart(state.cart)
-}
-
-export function invoice(state) {
-  return new CartItemList(state.invoice)
 }
--- src/store/Cart/state.js
+++ src/store/Cart/state.js
@@ -1,6 +1,5 @@
 export default function () {
   return {
-    cart: {},
-    invoice: []
+    cart: {}
   }
 }
