313f2e6f269814c8c62eee69245288a6099a9eef
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-08-13 19:41:52 +0430

    done and work well

--- src/components/FormBuilderCustumComponents/Map/LineType.vue
+++ src/components/FormBuilderCustumComponents/Map/LineType.vue
@@ -3,10 +3,11 @@
     <div v-if="label">
       {{ label }}
     </div>
+
     <q-option-group
-      class="flex"
-      v-model="LineType"
-      :options="LineTypeOptions"
+      v-model="inputData.dir"
+      :options="lineTypeOptions"
+      @update:model-value="onChangeLineType"
       color="primary"
       type="radio"
       dense
@@ -14,17 +15,17 @@
     />
   </div>
   <div
-    v-show="LineType !=='fixed'">
-    <div>
+    v-show="inputData.dir !=='fixed'">
+    <div v-if="inputData.style">
       <q-badge color="secondary">
-        سرعت حرکت : از {{ standard.min }} تا {{ standard.max }} ({{ min }} تا {{ max }})
+        سرعت حرکت : از 0 تا 100 ({{inputData.style['animation-duration'] }} )
       </q-badge>
-      <q-range
-        v-model="standard"
+      <q-slider
+        v-model="inputData.style['animation-duration']"
         :min="0"
         :max="100"
         :disable="disable"
-        @input="change($event)"
+        @input="onChangeStandard"
       />
     </div>
   </div>
@@ -37,12 +38,12 @@ export default {
   name: 'LineType',
   data () {
     return {
-      LineType: 'fixed',
+      lineType: 'flowing',
       standard: {
         min: 0,
         max: 17
       },
-      LineTypeOptions: [
+      lineTypeOptions: [
         {
           id: 0,
           label: 'بدون حرکت',
@@ -68,46 +69,27 @@ export default {
       type: String
     }
   },
-  watch: {
-    standard: {
-      handler (newValue) {
-        if (!newValue) {
-          return
-        }
-        this.setLineFlow()
-      }
-    },
-    LineType: {
-      handler (newValue) {
-        if (!newValue) {
-          return
-        }
-        this.setLineFlow()
-      }
-    }
-  },
   mixins: [inputMixin],
+  created () {
+  },
   methods: {
+    onChangeStandard () {
+      this.setLineFlow()
+    },
+    onChangeLineType () {
+      this.setLineFlow()
+    },
+    prepareDefaultInputData () {
+      if (this.inputData && this.inputData.style && typeof this.inputData.style['animation-duration'] !== 'undefined') {
+        return
+      }
+      this.inputData.style = {}
+      this.inputData.style['animation-duration'] = 0
+    },
     setLineFlow() {
-      if (this.LineType === 'fixed') {
-        this.inputData = {
-          dir: this.LineType,
-          style: {}
-        }
-      }
-      this.inputData = {
-        options: {
-          flowing: {
-            dir: this.LineType,
-            style: {
-              'animation-duration': this.standard.max - this.standard.min
-            }
-          }
-        }
-      }
+      this.prepareDefaultInputData()
       this.change(this.inputData)
     }
-
   }
 }
 </script>
