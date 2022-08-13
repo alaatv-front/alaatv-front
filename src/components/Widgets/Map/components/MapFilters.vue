103e780f144301c27a7904d9e19d40fc7521be16
Nima.mr <nima1380620@yahoo.com>
2022-08-13 18:53:10 +0430

    base map and map filters are updated!

--- src/components/Widgets/Map/components/MapFilters.vue
+++ src/components/Widgets/Map/components/MapFilters.vue
@@ -22,6 +22,18 @@ export default {
     }
   },
   methods: {
+    showOrHideFilters (filterClass, mode) {
+      this.filters.forEach(item => {
+        if (item.class !== filterClass) {
+          return null
+        }
+        item.show = mode === 'show'
+        if (item.separator === undefined) {
+          return null
+        }
+        item.separator = mode === 'show'
+      })
+    },
     initFilterData () {
       this.filters = [
         {
@@ -31,99 +43,205 @@ export default {
         }, {
           value: 'رشته_تجربی',
           label: 'رشته تجربی',
-          separator: true,
-          show: true
+          show: true,
+          separator: true
         }, {
           value: 'راه_ابریشم',
           label: 'راه ابریشم',
           show: true
         }, {
-          value: this.ocean,
+          value: 'اقیانوس',
           label: 'اقیانوس',
-          separator: true,
-          show: true
-        },
-        {
-          value: this.pille,
-          label: 'پیله',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang1,
-          label: 'فرسنگ۱',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang2,
-          label: 'فرسنگ 2',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang3,
-          label: 'فرسنگ 3',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: false,
-          label: 'فرسنگ 4',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang5,
-          label: 'فرسنگ 5',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang6,
-          label: 'فرسنگ 6',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang7,
-          label: 'فرسنگ 7',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang8,
-          label: 'فرسنگ 8',
-          show: this.showAbrishamFilters
-        },
-        {
-          value: this.farsang9,
-          label: 'فرسنگ 9',
-          show: this.showAbrishamFilters,
+          show: true,
           separator: true
+
         },
         {
-          value: this.riyazi,
+          class: 'abrisham',
+          value: 'پیله',
+          label: 'پیله',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_1',
+          label: 'فرسنگ 1',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_2',
+          label: 'فرسنگ 2',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_3',
+          label: 'فرسنگ 3',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_4',
+          label: 'فرسنگ 4',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_5',
+          label: 'فرسنگ 5',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_6',
+          label: 'فرسنگ 6',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_7',
+          label: 'فرسنگ 7',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_8',
+          label: 'فرسنگ 8',
+          show: false
+        },
+        {
+          class: 'abrisham',
+          value: 'فرسنگ_9',
+          label: 'فرسنگ 9',
+          show: false,
+          separator: null
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_1',
+          label: 'لنگرگاه 1',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_2',
+          label: 'لنگرگاه 2',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_3',
+          label: 'لنگرگاه 3',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_4',
+          label: 'لنگرگاه 4',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_5',
+          label: 'لنگرگاه 5',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_6',
+          label: 'لنگرگاه 6',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_7',
+          label: 'لنگرگاه 7',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_8',
+          label: 'لنگرگاه 8',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_9',
+          label: 'لنگرگاه 9',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_10',
+          label: 'لنگرگاه 10',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_11',
+          label: 'لنگرگاه 11',
+          show: false
+        },
+        {
+          class: 'ocean',
+          value: 'لنگرگاه_12',
+          label: 'لنگرگاه 12',
+          show: false,
+          separator: null
+
+        },
+        {
+          class: 'abrisham',
+          value: 'ریاضی',
           label: 'ریاضی',
-          show: this.showAbrishamFilters
+          show: false
         },
         {
-          value: this.fizik,
+          class: 'abrisham',
+          value: 'فیزیک',
           label: 'فیزیک',
-          show: this.showAbrishamFilters
+          show: false
         },
         {
-          value: this.shimi,
+          class: 'abrisham',
+          value: 'شیمی',
           label: 'شیمی',
-          show: this.showAbrishamFilters
+          show: false
         },
         {
-          value: this.zistShenasi,
+          class: 'abrisham',
+          value: 'زیست',
           label: 'زیست',
-          show: this.showAbrishamFilters
+          show: false
         }
       ]
     }
   },
-  watch: {
-    abrisham (newVal, OldVal) {
-      console.log('shit')
-      if (newVal === true) {
-        console.log('Moreshit')
-        this.showAbrishamFilters = true
+  computed: {
+    getFiltersObject () {
+      return (name) => {
+        this.filters.find((item) => {
+          if (item.label === name) {
+            return item
+          }
+        })
       }
+    }
+  },
+  watch: {
+    filterValues (newVal, OldVal) {
+      if (newVal.indexOf('راه_ابریشم') !== -1) {
+        this.showOrHideFilters('abrisham', 'show')
+      } else {
+        this.showOrHideFilters('abrisham', 'hide')
+      }
+      if (newVal.indexOf('اقیانوس') !== -1) {
+        this.showOrHideFilters('ocean', 'show')
+      } else {
+        this.showOrHideFilters('ocean', 'hide')
+      }
+      this.$emit('filterValues', newVal)
     }
   },
   created () {
