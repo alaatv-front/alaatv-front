ac57170ba2078363baea80122adcdba848de696d
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-08-15 17:48:50 +0430

    ItemEntity.vue done

--- src/components/FormBuilderCustumComponents/Map/ItemEntity.vue
+++ src/components/FormBuilderCustumComponents/Map/ItemEntity.vue
@@ -1,13 +1,23 @@
 <template>
   <q-option-group
-    class="flex"
+    v-model="inputData.entity_type"
     :options="options"
-    type="radio"
-    v-model="group"
+    @update:model-value="onChangeLineType"
     color="primary"
+    type="radio"
     dense
     inline
   />
+  <div
+    v-if="inputData.entity_type !== 'nothing'"
+  >
+
+    <q-input
+      v-model="inputData.altNames"
+      label="کد موجودیت را وارد کنید:"
+    />
+
+  </div>
 </template>
 
 <script>
@@ -16,27 +26,62 @@ export default {
   name: 'ItemEntity',
   data() {
     return {
-      group: [],
       options: [
         {
-          label: 'هیچکدام',
-          value: 0
+          entity_id: 0,
+          entity_type: 'nothing',
+          value: 'nothing',
+          display_name: 'هیچ کدام',
+          label: 'هیچ کدام',
+          altNames: []
         },
         {
+          entity_id: 1,
+          entity_type: 'product',
+          value: 'product',
+          display_name: 'محصول',
           label: 'محصول',
-          value: 1
+          altNames: []
         },
         {
+          entity_id: 2,
+          entity_type: 'set',
+          value: 'set',
+          display_name: 'دسته محتوا',
           label: 'دسته محتوا',
-          value: 2
+          altNames: []
         },
         {
-          label: ' محتوا',
-          value: 3
-        }]
+          entity_id: 3,
+          entity_type: 'content',
+          value: 'content',
+          display_name: 'محتوا',
+          label: 'محتوا',
+          altNames: []
+        }
+      ]
     }
   },
-  mixins: [inputMixin]
+  props: {
+    value: {
+      default: null
+    },
+    name: {
+      type: String,
+      default: 'ali'
+    }
+  },
+  mixins: [inputMixin],
+  methods: {
+    onChangeLineType () {
+      const name = this.options.find(item => item.entity_type === this.inputData.entity_type).display_name
+      const id = this.options.find(item => item.entity_type === this.inputData.entity_type).entity_id
+      const data = { entity_id: id, entity_type: this.inputData.entity_type, display_name: name, altNames: [] }
+      console.log(data)
+      this.change(data)
+      // this.setLineFlow()
+    }
+  }
 
 }
 </script>
