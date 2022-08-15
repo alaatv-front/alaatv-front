e41f8002803b9902712bcdbe959130bcb65a140c
Mhasanlookzade <hassan.lookzade@gmail.com>
2022-08-15 18:41:15 +0430

    z-index fixed

--- src/components/CustomDrawer.vue
+++ src/components/CustomDrawer.vue
@@ -193,7 +193,7 @@ export default {
     bottom: 0;
     height: 100%;
     width: 100%;
-    z-index: 9999;
+    z-index: 2000;
     overflow: auto;
     transition-property: transform;
     display: flex;
