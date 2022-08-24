0e027b28f4489768d1b88b7b9c0c547e215b73f7
Archer-Thane <bahrami.ehssan@gmail.com>
2022-08-24 17:30:44 +0430

    adding a route for form generator

---
+++ src/pages/formGenerator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div dir="ltr">
+    <form-builder-generator></form-builder-generator>
+  </div>
+</template>
+
+<script>
+import { FormBuilderGenerator } from 'quasar-form-builder'
+
+export default {
+  name: 'formGenerator',
+  components: {
+    FormBuilderGenerator
+  }
+}
+</script>
+
+<style></style>
