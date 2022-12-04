404d546a681b9a5f5ce1abbf70faebeca02073ce
Ayhanemoon <ayhan.samimi@example.com>
2022-12-04 16:26:05 +0330

    update page builder registration

--- src/boot/registerQPageBuilder.js
+++ src/boot/registerQPageBuilder.js
@@ -1,9 +1,23 @@
 import { boot } from 'quasar/wrappers'
-import VuePlugin from 'quasar-ui-q-page-builder'
+// import VuePlugin from 'quasar-ui-q-page-builder'
 import QPageBuilder from 'quasar-ui-q-page-builder/src/components/QPageBuilder.vue'
 import widgetList from '../components/Widgets/widget.json'
-export const widgetExpanded = VuePlugin.extractWidgets(widgetList)
 
+const widgets = []
+function extractWidgets(json) {
+  json.children.forEach(element => {
+    if (element.type === 'widget') {
+      widgets.push(element)
+    } else {
+      // eslint-disable-next-line no-prototype-builtins
+      if (element.hasOwnProperty('children')) {
+        extractWidgets(element)
+      }
+    }
+  })
+}
+extractWidgets(widgetList)
+export const widgetExpanded = widgets
 export default boot(({ app }) => {
   // app.use(VuePlugin)
   app.config.globalProperties.$q.$QPageBuilderWidgetList = widgetExpanded
