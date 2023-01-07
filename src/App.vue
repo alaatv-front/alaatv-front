2a139e1758764b76ae45d6bdfd458eb28669e5ad
kerasus <aliesmaeeli20@gmail.com>
2023-01-07 12:06:17 +0330

    updateAxiosBearerToken

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,27 @@ export default defineComponent({
     registration: null,
     updateExists: false
   }),
+  computed: {
+    accessToken () {
+      return this.$store.getters['Auth/accessToken']
+    }
+  },
   created () {
+    this.updateAxiosBearerToken()
     this.setServiceWorker()
     // this.checkInternetConnection()
   },
+  mounted () {
+    this.updateAxiosBearerToken()
+  },
   methods: {
+    // ToDo: must remove this function
+    updateAxiosBearerToken () {
+      if (!this.accessToken) {
+        return
+      }
+      this.$axios.defaults.headers.common.Authorization = 'Bearer ' + this.accessToken
+    },
     // async checkInternetConnection () {
     //   Network.addListener('networkStatusChange', (status) => {
     //     // alert("Network status changed" + status)
