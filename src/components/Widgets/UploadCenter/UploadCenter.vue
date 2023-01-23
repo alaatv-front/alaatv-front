40c231fd11556c3e4b83f8e54c91d18c0beb2b8d
Ayhanemoon <ayhan.samimi@example.com>
2023-01-23 12:57:13 +0330

    update upload center structure

---
+++ src/components/Widgets/UploadCenter/UploadCenter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <q-btn color="primary"
+           label="آپلود"
+           @click="toggleUploadProgressDialog" />
+    <upload-progress-dialog v-model:dialog="progressDialog" />
+  </div>
+</template>
+
+<script>
+import UploadProgressDialog from './components/UploadProgressDialog/UploadProgressDialog.vue'
+
+export default {
+  name: 'UploadCenterComponent',
+  components: {
+    UploadProgressDialog
+  },
+  data() {
+    return {
+      progressDialog: false
+    }
+  },
+  methods: {
+    toggleUploadProgressDialog() {
+      this.progressDialog = !this.progressDialog
+    }
+  }
+}
+</script>
