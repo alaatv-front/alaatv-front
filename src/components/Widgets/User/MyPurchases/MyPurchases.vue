9b46bcf5891a6c6ec25eb4884c9d891ab13ce4c9
kerasus <aliesmaeeli20@gmail.com>
2023-02-28 15:38:55 +0330

    user MyPurchases.vue

--- src/components/Widgets/User/MyPurchases/MyPurchases.vue
+++ src/components/Widgets/User/MyPurchases/MyPurchases.vue
@@ -108,11 +108,11 @@
 import { Set } from 'src/models/Set.js'
 import Assist from 'src/plugins/Assist.js'
 import Addresses from 'src/api/Addresses.js'
+import { APIGateway } from 'src/api/APIGateway.js'
 import { Product, ProductList } from 'src/models/Product.js'
-import FilterBox from 'components/userPurchases/filterBox.vue'
-import PurchaseItem from 'components/userPurchases/PurchaseItem.vue'
-import showContents from 'components/userPurchases/showContents.vue'
-import { APIGateway } from 'src/api/APIGateway'
+import FilterBox from 'src/components/userPurchases/filterBox.vue'
+import PurchaseItem from 'src/components/userPurchases/PurchaseItem.vue'
+import showContents from 'src/components/userPurchases/showContents.vue'
 
 export default {
   name: 'MyPurchases',
@@ -913,7 +913,6 @@ export default {
   },
   mounted () {
     this.filterProduct()
-    this.getFavored()
     this.getPurchasedProducts()
   },
   methods: {
@@ -924,9 +923,6 @@ export default {
     },
     getPurchasedProducts () {
       APIGateway.user.getPurchasedProducts()
-    },
-    getFavored () {
-      APIGateway.user.getFavored()
     },
     filterProduct () {
       this.filterProductByCategory()
