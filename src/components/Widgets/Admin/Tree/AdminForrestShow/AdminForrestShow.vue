a13dda6810468b442160c3b607b9120f0fd8304d
JasGH <yasaman.gonehi@gmail.com>
2023-03-01 15:42:29 +0330

    forrest edit & show updated

--- src/components/Widgets/Admin/Tree/AdminForrestShow/AdminForrestShow.vue
+++ src/components/Widgets/Admin/Tree/AdminForrestShow/AdminForrestShow.vue
@@ -1,20 +1,44 @@
 <template>
-  <entity-show v-model:value="inputs"
+  <entity-show ref="entityShow"
+               v-model:value="inputs"
                title="نمایش درخت"
                :api="getEntityShowAPI"
                :entity-param-key="entityParamKey"
                :entity-id-key="entityIdKey"
                :index-route-name="indexRouteName"
-               :edit-route-name="editRouteName" />
+               :edit-route-name="editRouteName"
+               :after-get-data="showForrest" />
+  <div class="relative-position">
+    <q-card class="tree-box q-my-md">
+      <q-card-section>نمایش ساختار درخت</q-card-section>
+      <tree v-show="!treeLoading"
+            ref="tree"
+            :key="treeKey"
+            :no-nodes-label="'درخت مورد نظر بارگیری نشده است.'"
+            :get-node-by-id="getNodeById"
+            :show-create-btn="false" />
+      <q-inner-loading :showing="treeLoading">
+        <q-spinner-ball color="primary"
+                        size="50px" />
+      </q-inner-loading>
+    </q-card>
+  </div>
+
 </template>
 
 <script>
 import { EntityShow } from 'quasar-crud'
 import { APIGateway } from 'src/api/APIGateway'
+import Tree from 'components/Widgets/Tree/Tree.vue'
+import { mixinTree, mixinWidget } from 'src/mixin/Mixins.js'
 
 export default {
   name: 'AdminForrestShow',
-  components: { EntityShow },
+  components: { Tree, EntityShow },
+  mixins: [
+    mixinTree,
+    mixinWidget
+  ],
   data () {
     return {
       api: APIGateway.forrest.FullAPIAdresses.base,
@@ -27,14 +51,47 @@ export default {
         { type: 'input', name: 'order', label: 'ترتیب', outlined: true, placeholder: ' ', responseKey: 'data.order', col: 'col-md-4' },
         { type: 'input', name: 'type', label: 'دسته', outlined: true, placeholder: ' ', responseKey: 'data.type', col: 'col-md-4' },
         { type: 'hidden', name: 'id', responseKey: 'data.id', col: 'col-md-12' }
-      ]
+      ],
+      treeKey: 0,
+      treeLoading: false
     }
   },
   computed: {
     getEntityShowAPI () {
-      return this.$apiGateway.forrest.FullAPIAdresses.grid(this.$route.params.id)
+      return this.$apiGateway.forrest.FullAPIAdresses.grid(this.getEntityId)
+    },
+    getEntityId () {
+      return this.$route.params.id
+    },
+    getTreeType () {
+      const typeInput = this.inputs.find(input => input.name === 'type')
+      return typeInput.value
     }
   },
-  methods: {}
+  updated () {
+    this.reloadTree()
+  },
+  methods: {
+    showForrest () {
+      this.treeLoading = true
+      this.showTree('tree', this.getRootNode(this.getTreeType))
+        .then(() => {
+          this.treeLoading = false
+        })
+        .catch(err => {
+          console.error(err)
+          this.treeLoading = false
+        })
+    },
+    reloadTree () {
+      this.treeKey++
+    }
+  }
 }
 </script>
+
+<style  lang="scss" scoped>
+.tree-box {
+  min-height: 150px;
+}
+</style>
