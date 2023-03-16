5c7cceef28cffe5a3287790d0e0f26841a348bf0
Ayhanemoon <ayhan.samimi@example.com>
2023-03-16 14:20:11 +0330

    set topics

--- src/store/ChatreNejat/getters.js
+++ src/store/ChatreNejat/getters.js
@@ -1,6 +1,15 @@
 const getters = {
   pageBuilderEditable: (state) => {
     return state.pageBuilderEditable
+  },
+  setList: (state) => {
+    return state.setList
+  },
+  setTopicList: (state) => {
+    return state.pageBuilderEditable
+  },
+  selectedTopic: (state) => {
+    return state.pageBuilderEditable
   }
 }
 
