8c6ccc51d7570144103ab54b41374ac6105a630d
kerasus <aliesmaeeli20@gmail.com>
2023-03-19 21:37:00 +0330

    debug Authenticated

--- src/router/middleware/Authenticated.js
+++ src/router/middleware/Authenticated.js
@@ -1,5 +1,6 @@
 export default function Authenticated ({ next, store, to }) {
   const accessToken = store.$accessToken
+  console.log('accessToken', accessToken)
   if (!accessToken) {
     store.commit('Auth/updateRedirectTo', { name: to.name, params: to.params })
     return next({ name: 'login' })
