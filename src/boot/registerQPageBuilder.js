ff2ff47f29d912163fb87cb70d2dfa6779cffb84
kerasus <aliesmaeeli20@gmail.com>
2023-03-26 18:02:11 +0330

    page builder option panel bug fix

--- src/boot/registerQPageBuilder.js
+++ src/boot/registerQPageBuilder.js
@@ -19,61 +19,6 @@ function extractWidgets (json) {
 extractWidgets(widgetList)
 export const widgetExpanded = widgets
 export default boot(async function ({ app }) {
-  // const widgetExpanded = VuePlugin.install(app, widgetList)
-
-  // async function registerWidgets (widgetsList) {
-  //   const components = {}
-  //   const optionPanels = {}
-  //   const widgetsListLength = widgetsList.length
-  //   for (let i = 0; i < widgetsListLength; i++) {
-  //     const element = widgetsList[i]
-  //     const widgetComponentName = element.name
-  //     const widgetComponentPath = element.path + '/' + widgetComponentName
-  //     const widgetOptionPanelPath = element.path + '/' + 'OptionPanel'
-  //     const widgetComponentPathInArray = widgetComponentPath.split('/')
-  //
-  //     if (widgetComponentPathInArray[widgetComponentPathInArray.length - 1] !== widgetComponentPathInArray[widgetComponentPathInArray.length - 2]) {
-  //       continue
-  //     }
-  //     // :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :(
-  //     // https://github.com/vitejs/vite/issues/4945#issuecomment-951770052
-  //     if (widgetComponentPathInArray.length === 1) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}.vue`)).default
-  //     }
-  //     if (widgetComponentPathInArray.length === 2) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}/${widgetComponentPathInArray[1]}.vue`)).default
-  //     }
-  //     if (widgetComponentPathInArray.length === 3) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}/${widgetComponentPathInArray[1]}/${widgetComponentPathInArray[2]}.vue`)).default
-  //     }
-  //     if (widgetComponentPathInArray.length === 4) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}/${widgetComponentPathInArray[1]}/${widgetComponentPathInArray[2]}/${widgetComponentPathInArray[3]}.vue`)).default
-  //     }
-  //     if (widgetComponentPathInArray.length === 5) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}/${widgetComponentPathInArray[1]}/${widgetComponentPathInArray[2]}/${widgetComponentPathInArray[3]}/${widgetComponentPathInArray[4]}.vue`)).default
-  //     }
-  //     if (widgetComponentPathInArray.length === 6) {
-  //       components[widgetComponentName] = (await import(`../${widgetComponentPathInArray[0]}/${widgetComponentPathInArray[1]}/${widgetComponentPathInArray[2]}/${widgetComponentPathInArray[3]}/${widgetComponentPathInArray[4]}/${widgetComponentPathInArray[5]}.vue`)).default
-  //     }
-  //     // https://github.com/vitejs/vite/issues/4945#issuecomment-951770052
-  //     // :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :(
-  //
-  //     if (element.optionPanel) {
-  //       optionPanels[widgetComponentName] = {
-  //         name: widgetComponentName,
-  //         tagName: widgetComponentName[0].toLowerCase() + widgetComponentName.slice(1, widgetComponentName.length).replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`),
-  //         path: '../' + widgetOptionPanelPath + '.vue'
-  //       }
-  //     }
-  //   }
-  //
-  //   app.config.globalProperties.$q.$pageBuilderWidgetComponents = components
-  //   app.config.globalProperties.$q.$pageBuilderWidgetOptionPanels = optionPanels
-  // }
-
-  // await registerWidgets(widgetExpanded)
-
-  // app.use(VuePlugin)
   app.config.globalProperties.$q.$QPageBuilderWidgetList = widgetExpanded
 
   async function registerWidgets (widgetsList) {
@@ -86,6 +31,7 @@ export default boot(async function ({ app }) {
       const widgetComponentPath = element.path + '/' + widgetComponentName
       const widgetOptionPanelPath = element.path + '/' + 'OptionPanel'
       const widgetComponentPathInArray = widgetComponentPath.split('/')
+      const widgetOptionPanelPathInArray = widgetOptionPanelPath.split('/')
 
       if (widgetComponentPathInArray[widgetComponentPathInArray.length - 1] !== widgetComponentPathInArray[widgetComponentPathInArray.length - 2]) {
         continue
@@ -114,11 +60,30 @@ export default boot(async function ({ app }) {
       // :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :(
 
       if (element.optionPanel) {
-        optionPanels[widgetComponentName] = {
-          name: widgetComponentName,
-          tagName: widgetComponentName[0].toLowerCase() + widgetComponentName.slice(1, widgetComponentName.length).replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`),
-          path: '../' + widgetOptionPanelPath + '.vue'
+        const optionPanelWidgetName = widgetComponentName + 'OptionPanel'
+        // :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :(
+        // https://github.com/vitejs/vite/issues/4945#issuecomment-951770052
+        if (widgetOptionPanelPathInArray.length === 1) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}.vue`)).default
         }
+        if (widgetOptionPanelPathInArray.length === 2) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}/${widgetOptionPanelPathInArray[1]}.vue`)).default
+        }
+        if (widgetOptionPanelPathInArray.length === 3) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}/${widgetOptionPanelPathInArray[1]}/${widgetOptionPanelPathInArray[2]}.vue`)).default
+        }
+        if (widgetOptionPanelPathInArray.length === 4) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}/${widgetOptionPanelPathInArray[1]}/${widgetOptionPanelPathInArray[2]}/${widgetOptionPanelPathInArray[3]}.vue`)).default
+        }
+        if (widgetOptionPanelPathInArray.length === 5) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}/${widgetOptionPanelPathInArray[1]}/${widgetOptionPanelPathInArray[2]}/${widgetOptionPanelPathInArray[3]}/${widgetOptionPanelPathInArray[4]}.vue`)).default
+        }
+        if (widgetOptionPanelPathInArray.length === 6) {
+          optionPanels[optionPanelWidgetName] = (await import(`../${widgetOptionPanelPathInArray[0]}/${widgetOptionPanelPathInArray[1]}/${widgetOptionPanelPathInArray[2]}/${widgetOptionPanelPathInArray[3]}/${widgetOptionPanelPathInArray[4]}/${widgetOptionPanelPathInArray[5]}.vue`)).default
+        }
+        // https://github.com/vitejs/vite/issues/4945#issuecomment-951770052
+        // :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :(
+
       }
     }
 
