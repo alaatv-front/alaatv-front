ea7d9d9f391afadd1f7ad1120dfde77927a4df3b
JasGH <yasaman.gonehi@gmail.com>
2023-03-27 19:40:34 +0330

    add favored route to set model

--- src/api/models/set.js
+++ src/api/models/set.js
@@ -8,6 +8,8 @@ const urlAddress = {
   adminBase: '/admin/set',
   adminShow: (id) => '/admin/set/' + id,
   attachContents: (setId) => '/admin/set/' + setId + '/c/attach',
+  favored: (id) => '/set/' + id + '/favored',
+  unfavored: (id) => '/set/' + id + '/unfavored',
   create: '/admin/set/',
   show: (id) => '/set/' + id,
   getContents: (id) => '/set/' + id + '/contents'
@@ -19,7 +21,9 @@ export default class SetAPI extends APIRepository {
       base: this.name + this.APIAdresses.base,
       show: (id) => this.name + this.APIAdresses.show(id),
       getContents: (id) => this.name + this.APIAdresses.getContents(id),
-      attachContents: (setId) => this.name + this.APIAdresses.attachContents(setId)
+      attachContents: (setId) => this.name + this.APIAdresses.attachContents(setId),
+      favored: id => this.name + this.APIAdresses.favored(id),
+      unfavored: id => this.name + this.APIAdresses.unfavored(id)
     }
     this.restUrl = (id) => this.APIAdresses.base + '/' + id
     /* Setting the callback functions for the CRUD operations. */
@@ -76,6 +80,38 @@ export default class SetAPI extends APIRepository {
         return error
       },
       data
+    })
+  }
+
+  favored(data = {}, cache = { TTL: 100 }) {
+    return this.sendRequest({
+      apiMethod: 'post',
+      api: this.api,
+      request: this.APIAdresses.favored(data),
+      cacheKey: this.CacheList.favored(data),
+      ...(cache !== undefined && { cache }),
+      resolveCallback: (response) => {
+        return response.data
+      },
+      rejectCallback: (error) => {
+        return error
+      }
+    })
+  }
+
+  unfavored(data = {}, cache = { TTL: 100 }) {
+    return this.sendRequest({
+      apiMethod: 'post',
+      api: this.api,
+      request: this.APIAdresses.unfavored(data),
+      cacheKey: this.CacheList.unfavored(data),
+      ...(cache !== undefined && { cache }),
+      resolveCallback: (response) => {
+        return response.data
+      },
+      rejectCallback: (error) => {
+        return error
+      }
     })
   }
 }
