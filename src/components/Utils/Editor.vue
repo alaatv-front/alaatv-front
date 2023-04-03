502bc9c766ed51df703b99ec18faf6832604b06a
JasGH <yasaman.gonehi@gmail.com>
2023-04-03 17:21:36 +0330

    editor update

--- src/components/Utils/Editor.vue
+++ src/components/Utils/Editor.vue
@@ -5,7 +5,6 @@
             :toolbar="[
               [
                 {
-                  label: $q.lang.editor.align,
                   icon: $q.iconSet.editor.align,
                   fixedLabel: true,
                   list: 'only-icons',
@@ -14,28 +13,40 @@
               ],
               [
                 {
-                  label: 'فرمت نوشتار',
                   icon: 'isax:firstline',
                   fixedLabel: true,
                   list: 'only-icons',
                   options: ['bold', 'italic', 'strike', 'underline']
                 }
               ],
-              ['token'],
               [
                 {
-                  label: 'فهرست بندی',
                   icon: 'isax:document',
                   fixedLabel: true,
                   list: 'only-icons',
                   options: [ 'unordered', 'ordered']
                 }
               ],
+              [ 'token'],
               [{
-                label: $q.lang.editor.formatting,
                 icon: $q.iconSet.editor.formatting,
                 list: 'no-icons',
                 options: ['p', 'h3', 'h4', 'h5', 'h6', 'code']
+              }],
+              [{
+                icon: $q.iconSet.editor.fontSize,
+                fixedLabel: true,
+                fixedIcon: true,
+                list: 'no-icons',
+                options: [
+                  'size-1',
+                  'size-2',
+                  'size-3',
+                  'size-4',
+                  'size-5',
+                  'size-6',
+                  'size-7'
+                ]
               }],
               ['viewsource']
             ]">
@@ -43,9 +54,7 @@
       <q-btn-dropdown ref="colorPicker"
                       dense
                       unelevated
-                      color="white"
                       icon="colorize"
-                      text-color="primary"
                       size="sm">
         <q-list dense>
           <q-item tag="label"
@@ -68,9 +77,9 @@ export default {
   name: 'Editor',
   props: {
     value: {
-      type: Object,
+      type: String,
       default() {
-        return {}
+        return null
       }
     }
   },
