64bb42b7845e3dd2e871f47e86875ee25b15b3cf
JasGH <yasaman.gonehi@gmail.com>
2023-04-03 17:31:46 +0330

    text widget option panel update

--- src/components/Widgets/TextWidget/OptionPanel.vue
+++ src/components/Widgets/TextWidget/OptionPanel.vue
@@ -2,66 +2,29 @@
   <option-panel-tabs v-model:options="localOptions">
     <template #main-tab>
       <div class="option-panel-container">
-        <div class="row">
+        <div class="row q-mt-sm">
           <div class="col-md-12">
-            <q-editor ref="editor"
-                      v-model="localOptions.text"
-                      min-height="10rem"
-                      :toolbar="[
-                        [
-                          {
-                            label: $q.lang.editor.align,
-                            icon: $q.iconSet.editor.align,
-                            fixedLabel: true,
-                            list: 'only-icons',
-                          },
-                        ],
-                        ['bold', 'italic', 'strike', 'underline', 'token'],
-                        ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
-                        ['viewsource']
-                      ]">
-              <template v-slot:token>
-                <q-btn-dropdown ref="token"
-                                dense
-                                unelevated
-                                color="white"
-                                icon="colorize"
-                                text-color="primary"
-                                size="sm">
-                  <q-list dense>
-                    <q-item tag="label"
-                            clickable>
-                      <q-item-section>
-                        <q-btn icon="check"
-                               @click="color('foreColor', foreColor)" />
-                        <q-color v-model="foreColor"
-                                 class="my-picker" />
-                      </q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-btn-dropdown>
-              </template>
-            </q-editor>
+            <editor v-model:value="localOptions.text" />
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 q-ma-sm">
             <q-select v-model="responsive"
                       :options="responsiveOpts"
                       label="responsive" />
           </div>
-          <div class="col-md-3 offset-1">
+          <div class="col-md-3 offset-1 q-ma-sm">
             <q-select v-model="localOptions[responsive].fontStyle"
                       :options="fontStyle"
                       label="fontStyle" />
           </div>
-          <div class="col-md-3 offset-1">
+          <div class="col-md-3 offset-1 q-ma-sm">
             <q-input v-model="localOptions.fontFamily"
                      label="font family" />
           </div>
-          <div class="col-md-3">
+          <div class="col-md-3 q-ma-sm">
             <q-input v-model="localOptions[responsive].fontSize"
                      label="font size" />
           </div>
-          <div class="col-md-3 offset-1">
+          <div class="col-md-3 offset-1 q-ma-sm">
             <q-input v-model="localOptions[responsive].fontWeight"
                      label="font weight" />
           </div>
@@ -74,10 +37,11 @@
 import { defineComponent } from 'vue'
 import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
 import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+import Editor from 'components/Utils/Editor.vue'
 
 export default defineComponent({
   name: 'OptionPanel',
-  components: { OptionPanelTabs },
+  components: { Editor, OptionPanelTabs },
   mixins: [mixinOptionPanel],
   props: {
     options: {
