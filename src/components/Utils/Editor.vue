a44ae4c6f41a5a0b3729d1eb682637cdbe2f5bf9
JasGH <yasaman.gonehi@gmail.com>
2023-04-03 17:29:24 +0330

    editor update

--- src/components/Utils/Editor.vue
+++ src/components/Utils/Editor.vue
@@ -9,45 +9,41 @@
                   fixedLabel: true,
                   list: 'only-icons',
                   options: ['left', 'center', 'right', 'justify']
-                }
-              ],
-              [
+                },
                 {
                   icon: 'isax:firstline',
                   fixedLabel: true,
                   list: 'only-icons',
                   options: ['bold', 'italic', 'strike', 'underline']
-                }
-              ],
-              [
+                },
                 {
                   icon: 'isax:document',
                   fixedLabel: true,
                   list: 'only-icons',
                   options: [ 'unordered', 'ordered']
+                },
+                'token',
+                {
+                  icon: $q.iconSet.editor.formatting,
+                  list: 'no-icons',
+                  options: ['p', 'h3', 'h4', 'h5', 'h6', 'code']
+                },
+                {
+                  icon: $q.iconSet.editor.fontSize,
+                  fixedLabel: true,
+                  fixedIcon: true,
+                  list: 'no-icons',
+                  options: [
+                    'size-1',
+                    'size-2',
+                    'size-3',
+                    'size-4',
+                    'size-5',
+                    'size-6',
+                    'size-7'
+                  ]
                 }
               ],
-              [ 'token'],
-              [{
-                icon: $q.iconSet.editor.formatting,
-                list: 'no-icons',
-                options: ['p', 'h3', 'h4', 'h5', 'h6', 'code']
-              }],
-              [{
-                icon: $q.iconSet.editor.fontSize,
-                fixedLabel: true,
-                fixedIcon: true,
-                list: 'no-icons',
-                options: [
-                  'size-1',
-                  'size-2',
-                  'size-3',
-                  'size-4',
-                  'size-5',
-                  'size-6',
-                  'size-7'
-                ]
-              }],
               ['viewsource']
             ]">
     <template v-slot:token>
