e593093422cc5289055ff442406bcb86daf0df8b
JasGH <yasaman.gonehi@gmail.com>
2023-04-04 14:12:02 +0330

    action button option panel

---
+++ src/components/Widgets/ActionButton/OptionPanel.vue
@@ -0,0 +1,69 @@
+<template>
+  <option-panel-tabs v-model:options="localOptions">
+    <template #main-tab>
+      <div class="option-panel-container q-py-md">
+        <div class="row q-gutter-md">
+          <div class="input-container">
+            <div class="outsideLabel">label</div>
+            <q-input v-model="value.label"
+                     label="label" />
+          </div>
+          <div class="input-container">
+            <div class="outsideLabel">icon name</div>
+            <q-input v-model="value.icon"
+                     label="icon name" />
+          </div>
+        </div>
+      </div>
+    </template>
+  </option-panel-tabs>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
+import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+
+export default defineComponent({
+  name: 'OptionPanel',
+  components: { OptionPanelTabs },
+  mixins: [mixinOptionPanel],
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      data: [
+        'accent',
+        'dark',
+        'positive',
+        'customGray',
+        'primary'
+      ]
+    }
+  },
+  computed: {
+    value: {
+      get() {
+        return this.localOptions
+      },
+      set(value) {
+        this.localOptions = value
+      }
+    }
+  },
+  watch: {
+    localOptions: {
+      handler(newVal) {
+        this.$emit('update:options', newVal)
+      },
+      deep: true
+    }
+  },
+  methods: {}
+})
+</script>
