cfd8685c67e1cfb0f6ddc6100834c7aeae37061e
JasGH <yasaman.gonehi@gmail.com>
2023-04-05 13:36:22 +0330

    ImageWidget option panel update

---
+++ src/components/Widgets/ImageWidget/OptionPanel.vue
@@ -0,0 +1,45 @@
+<template>
+  <option-panel-tabs v-model:options="localOptions">
+    <template #main-tab>
+      <div class="option-panel-container">
+        <div class="input-container q-py-md">
+          <div class="outsideLabel">لینک عکس</div>
+          <q-input v-model="localOptions.imageSource"
+                   label="image" />
+        </div>
+        <div class="input-container q-py-md">
+          <div class="outsideLabel">height</div>
+          <q-input v-model="localOptions.height"
+                   label="image" />
+        </div>
+      </div>
+    </template>
+  </option-panel-tabs>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
+import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+
+export default defineComponent({
+  name: 'OptionPanel',
+  components: { OptionPanelTabs },
+  mixins: [mixinOptionPanel],
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  watch: {
+    localOptions: {
+      handler(newVal) {
+        this.$emit('update:options', newVal)
+      },
+      deep: true
+    }
+  }
+})
+</script>
