a9c84878b70b786ef67658d7c0d717ed0f4220f9
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-04-29 18:54:17 +0330

    typography

---
+++ src/components/Theme/Typography/Typography.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="row justify-center q-my-md">
+    <div class="col-md-9">
+      <h2>
+        Typography
+      </h2>
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5">
+          <h1>
+            مدرسه آنلاین آلا
+          </h1>
+        </div>
+        <div class="col-md-5">
+          <h1>
+            IransansXfaNum - 48 Bold
+          </h1>
+        </div>
+        <div class="col-md-2">
+          <h1>
+            h1
+          </h1>
+        </div>
+      </div>
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5">
+          <h2>
+            مدرسه آنلاین آلا
+          </h2>
+        </div>
+        <div class="col-md-5">
+          <h2>
+            IransansXfaNum - 36 demibold
+          </h2>
+        </div>
+        <div class="col-md-2">
+          <h2>
+            h2
+          </h2>
+        </div>
+      </div>
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5">
+          <h3>
+            مدرسه آنلاین آلا
+          </h3>
+        </div>
+        <div class="col-md-5">
+          <h3>
+            IransansXfaNum - 28 demibold
+          </h3>
+        </div>
+        <div class="col-md-2">
+          <h3>
+            h3
+          </h3>
+        </div>
+      </div>
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5">
+          <h4>
+            مدرسه آنلاین آلا
+          </h4>
+        </div>
+        <div class="col-md-5">
+          <h4>
+            IransansXfaNum - 20 demibold
+          </h4>
+        </div>
+        <div class="col-md-2">
+          <h4>
+            h4
+          </h4>
+        </div>
+      </div>
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5">
+          <p class="p1">
+            مدرسه آنلاین آلا
+          </p>
+        </div>
+        <div class="col-md-5">
+          <p class="p1">
+            IransansXfaNum - 14 regular
+          </p>
+        </div>
+        <div class="col-md-2">
+          <p class="p1">
+            P1
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typography'
+}
+</script>
+
+<style scoped>
+
+</style>
