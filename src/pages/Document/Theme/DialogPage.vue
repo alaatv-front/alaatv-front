24cdd7ce37b0c13b8f8d4039d182eb322cc50a58
JasGH <yasaman.gonehi@gmail.com>
2023-05-01 14:59:47 +0330

    Dialog design

---
+++ src/pages/Document/Theme/DialogPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <example-dialog />
+  </div>
+</template>
+
+<script>
+import ExampleDialog from 'components/Theme/Dialog/ExampleDialog.vue'
+export default {
+  name: 'DialogPage',
+  components: { ExampleDialog }
+}
+</script>
+
+<style scoped>
+
+</style>
