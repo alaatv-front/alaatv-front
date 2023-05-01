6dcce1b9d653645c3d19fc50a73b337e46a62686
kerasus <aliesmaeeli20@gmail.com>
2023-05-01 13:24:15 +0330

    refactor widgets

--- src/components/Widgets/Content/Search/ContentSearch/components/SpeciferType.vue
+++ src/components/Widgets/Content/Search/ContentSearch/components/SpeciferType.vue
@@ -6,8 +6,8 @@
 <script>
 import { Content } from 'src/models/Content.js'
 import { Product } from 'src/models/Product.js'
-import ContentItem from 'src/components/Widgets/Content/Search/ContentItem/ContentItem.vue'
-import ProductItem from 'src/components/Widgets/Content/Search/ProductItem/ProductItem.vue'
+import ContentItem from 'components/Widgets/Content/Search/ContentSearch/components/ContentItem.vue'
+import ProductItem from 'components/Widgets/Content/Search/ContentSearch/components/ProductItem.vue'
 export default {
   name: 'MainContentBox',
   components: { ProductItem, ContentItem },
