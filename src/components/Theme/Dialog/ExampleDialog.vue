8c0f967af0bdfbe2c6f4146bfe1f9624efe2c7c1
JasGH <yasaman.gonehi@gmail.com>
2023-05-05 18:59:38 +0330

    design-system dialog

--- src/components/Theme/Dialog/ExampleDialog.vue
+++ src/components/Theme/Dialog/ExampleDialog.vue
@@ -6,8 +6,40 @@
       <q-btn color="primary"
              @click="dialogValue[item.size] = true">Open Dialog {{item.size}}</q-btn>
       <q-dialog v-model="dialogValue[item.size]">
-        <q-card>
-          ;sdolk;sflk
+        <q-card :class="item.cardClass">
+          <q-card-section class="dialog-header">
+            <q-avatar :class="'avatar-'+item.size"
+                      class="dialog-avatar"
+                      color="positive"
+                      text-color="white"
+                      icon="check" />
+            <h5 class="dialog-header-title">عنوان متن</h5>
+            <q-btn class="dialog-close-icon"
+                   icon="close"
+                   flat
+                   unelevated />
+          </q-card-section>
+          <q-separator class="grey1" />
+          <q-card-section class="dialog-body">
+            <h5>آیا می‌خواهید لورم ایپسوم شوید؟</h5>
+          </q-card-section>
+          <q-card-section class="dialog-action">
+            <q-btn class="q-btn-md dialog-action-default-btn"
+                   flat>
+              متن دکمه
+            </q-btn>
+            <div class="dialog-action-btn-box">
+              <q-btn class="q-btn-md q-mr-sm"
+                     color="secondary"
+                     outline>
+                انصراف
+              </q-btn>
+              <q-btn class="q-btn-md"
+                     color="positive">
+                تایید
+              </q-btn>
+            </div>
+          </q-card-section>
         </q-card>
       </q-dialog>
     </div>
