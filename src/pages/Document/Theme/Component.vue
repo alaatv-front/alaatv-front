b14127adc2c8a74c398f3dccea774c432fc4d83f
JasGH <yasaman.gonehi@gmail.com>
2023-05-06 19:51:02 +0330

    shadow

--- src/pages/Document/Theme/Component.vue
+++ src/pages/Document/Theme/Component.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="Shadow">
+    Shadow
+    <example-shadow />
+  </div>
   <div class="dialog">
     dialog & BottomSheet
     <example-dialog />
@@ -9,8 +13,9 @@
 <script>
 import ExampleDialog from 'components/Theme/Dialog/ExampleDialog.vue'
 import ExampleBottomSheet from 'components/Theme/Dialog/ExampleBottomSheet.vue'
+import ExampleShadow from 'components/Theme/Controls/Shadow/Shadow.vue'
 export default {
   name: 'Component',
-  components: { ExampleBottomSheet, ExampleDialog }
+  components: { ExampleShadow, ExampleBottomSheet, ExampleDialog }
 }
 </script>
