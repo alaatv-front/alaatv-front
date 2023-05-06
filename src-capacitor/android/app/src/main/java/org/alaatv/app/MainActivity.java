94f4673c18f0f4cd548e42fddfa775804675a419
MJavad <javadhosseini14@gmail.com>
2023-05-06 11:59:17 +0330

    StatusBar Issue resolved .

--- src-capacitor/android/app/src/main/java/org/alaatv/app/MainActivity.java
+++ src-capacitor/android/app/src/main/java/org/alaatv/app/MainActivity.java
@@ -1,6 +1,11 @@
 package org.alaatv.app;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.core.content.ContextCompat;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -17,5 +22,22 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+
+    setStatusBarColor();
+
+//    getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.status_bar));
+  }
+
+  public void setStatusBarColor(){
+    Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+    window.setStatusBarColor(ContextCompat.getColor(this,R.color.status_bar));
   }
 }
