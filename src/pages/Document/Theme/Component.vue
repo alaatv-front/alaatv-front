8751ecbe98c5d24dda142daa063b3432b4f4845d
JasGH <yasaman.gonehi@gmail.com>
2023-05-06 18:53:07 +0330

    spacing

--- src/pages/Document/Theme/Component.vue
+++ src/pages/Document/Theme/Component.vue
@@ -4,13 +4,18 @@
     <example-dialog />
     <example-bottom-sheet />
   </div>
+  <div class="spacing">
+    Spacing
+    <spacing />
+  </div>
 </template>
 
 <script>
 import ExampleDialog from 'components/Theme/Dialog/ExampleDialog.vue'
 import ExampleBottomSheet from 'components/Theme/Dialog/ExampleBottomSheet.vue'
+import Spacing from 'components/Theme/Spacing/Spacing.vue'
 export default {
   name: 'Component',
-  components: { ExampleBottomSheet, ExampleDialog }
+  components: { Spacing, ExampleBottomSheet, ExampleDialog }
 }
 </script>
