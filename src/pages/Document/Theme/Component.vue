c80ca23ef9679ff47ade9ffb94376244c2313d7e
JasGH <yasaman.gonehi@gmail.com>
2023-05-06 20:52:07 +0330

    ExampleTabs

--- src/pages/Document/Theme/Component.vue
+++ src/pages/Document/Theme/Component.vue
@@ -4,13 +4,18 @@
     <example-dialog />
     <example-bottom-sheet />
   </div>
+  <div class="tabs">
+    tabs
+    <example-tabs />
+  </div>
 </template>
 
 <script>
 import ExampleDialog from 'components/Theme/Dialog/ExampleDialog.vue'
 import ExampleBottomSheet from 'components/Theme/Dialog/ExampleBottomSheet.vue'
+import ExampleTabs from 'components/Theme/Tabs/ExampleTabs.vue'
 export default {
   name: 'Component',
-  components: { ExampleBottomSheet, ExampleDialog }
+  components: { ExampleTabs, ExampleBottomSheet, ExampleDialog }
 }
 </script>
