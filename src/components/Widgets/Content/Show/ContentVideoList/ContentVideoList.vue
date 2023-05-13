96a4dd381144d4735c309366c7c0e781b7779d14
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-05-13 11:38:07 +0330

    pamphlet

--- src/components/Widgets/Content/Show/ContentVideoList/ContentVideoList.vue
+++ src/components/Widgets/Content/Show/ContentVideoList/ContentVideoList.vue
@@ -17,6 +17,7 @@
                target="_blank">
               <q-btn icon="isax:document-download"
                      flat
+                     :disable="content.can_see"
                      color="primary"
                      size="13px" />
             </a>
