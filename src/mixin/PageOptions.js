e3145c5c3f89ce65c14e15216742b040618e67b6
kerasus <aliesmaeeli20@gmail.com>
2023-05-20 16:54:06 +0330

    check landing error in production

--- src/mixin/PageOptions.js
+++ src/mixin/PageOptions.js
@@ -44,9 +44,11 @@ const mixinPageOptions = {
       return this.getPageConfigRequest()
     },
     prefetchServerDataPromiseThen (pageSetting) {
+      console.warn('prefetchServerDataPromiseThen: ', pageSetting)
       const sections = pageSetting.value.sections
       const seo = pageSetting.value.seo
 
+      console.warn('PageBuilder sections: ', sections)
       // PageBuilder
       this.$store.commit('PageBuilder/updateCurrentSections', sections)
 
@@ -63,14 +65,17 @@ const mixinPageOptions = {
 
       this.$store.commit('PageBuilder/updatePageDataLoaded', true)
     },
-    prefetchServerDataPromiseCatch () {
+    prefetchServerDataPromiseCatch (error) {
+      console.warn('prefetchServerDataPromiseCatch: ', error)
       this.pageBuilderLoading = false
     },
     getPageConfigRequest() {
+      console.warn('getPageConfigRequest')
       this.pageBuilderLoading = true
       const params = JSON.stringify(this.$route.params)
       const routeName = this.$route.name
       const key = 'route_name:' + routeName + (this.hasDynamicSettingWithParams ? ('-params:' + params) : '')
+      console.warn('APIGateway.pageSetting.get(key): ', key)
       return APIGateway.pageSetting.get(key)
     }
   }
