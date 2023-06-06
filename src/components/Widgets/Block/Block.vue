c6d8dc2f37993ee072f148015c4bf2e0f459e311
JasGH <81778219+JasGH@users.noreply.github.com>
2023-06-06 12:35:21 +0330

    Bug fixes (#549)
    * style bugfix
    
    * style bugfix
    
    * style bugfix
    
    * style bugfix
    
    * style bugfix
    
    * style bugfix
    
    * bugfix
    
    * block update

--- src/components/Widgets/Block/Block.vue
+++ src/components/Widgets/Block/Block.vue
@@ -17,7 +17,7 @@
       <slider v-if="localOptions.block.banners && localOptions.block.banners.list.length > 0"
               :options="bannerSlides" />
       <div v-if="localOptions.block.products.list.length > 0"
-           class="row item-container q-col-gutter-md full-width"
+           class="row item-container q-col-gutter-md"
            :class="isGridView ? 'grid_view' : 'scroll-view'">
         <div v-for="product in localOptions.block.products.list"
              :key="product.id"
@@ -26,7 +26,7 @@
                'col-xl-3 col-lg-3 col-md-4 col-sm-5 col-xs-9': !isGridView,
              }"
              class="product-spacing">
-          <product-item :options="{product, minWidth: productItemMinWidth}" />
+          <product-item :options="{product, minWidth: productItemMinWidth, ...defaultOptions.productItemOptions}" />
         </div>
         <div v-if="localOptions.block?.url?.web"
              class="block-item-box">
@@ -38,7 +38,7 @@
         </div>
       </div>
       <div v-if="localOptions.block.sets.list.length > 0"
-           class="row item-container q-col-gutter-md full-width"
+           class="row item-container q-col-gutter-md"
            :class="isGridView ? 'grid_view' : 'scroll-view'">
         <div v-for="set in localOptions.block.sets.list"
              :key="set.id"
@@ -58,7 +58,7 @@
         </div>
       </div>
       <div v-if="localOptions.block.contents.list.length > 0"
-           class="row item-container q-col-gutter-md full-width"
+           class="row item-container q-col-gutter-md"
            :class="isGridView ? 'grid_view' : 'scroll-view'">
         <div v-for="content in localOptions.block.contents.list"
              :key="content.id"
@@ -106,6 +106,7 @@ export default {
       block: new Block(),
       gridView: false,
       showContentDownloadMenu: false,
+      productItemOptions: {},
       contentMinWidth: {
         inGridView: 'auto',
         inScrollView: 'auto'
@@ -262,6 +263,7 @@ export default {
     }
 
     .item-container {
+      width: 100%;
       &.grid_view {
         justify-content: center;
       }
