99ac00c21e343ebc78fbe6fabeef7d62732b7a89
kerasus <aliesmaeeli20@gmail.com>
2023-06-14 18:20:54 +0330

    abrishamPro

---
+++ src/pages/User/DashboardAbrishamPro/progress.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page-builder v-model:sections="sections"
+                  v-model::options="pageConfig" />
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data: () => ({
+    pageConfig: {},
+    sections: [
+      {
+        data: {
+          rows: [
+            {
+              cols: [
+                {
+                  widgets: [
+                    {
+                      name: 'abrisham-progress'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
