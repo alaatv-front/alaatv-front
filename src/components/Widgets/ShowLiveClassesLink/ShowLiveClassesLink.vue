a590e405bdf4c08ccb97659bb6a414d765af414b
ali_khoshnevisan <102961740+ali-khoshnevisan@users.noreply.github.com>
2023-06-26 18:12:56 +0330

    Live products modal route (#600)
    * services style
    
    * live products
    
    * live products

--- src/components/Widgets/ShowLiveClassesLink/ShowLiveClassesLink.vue
+++ src/components/Widgets/ShowLiveClassesLink/ShowLiveClassesLink.vue
@@ -26,20 +26,41 @@
             </template>
             <template v-else-if="products.list.length > 0">
               <div v-for="product in products.list"
-                   :key="product.id"
-                   class="col-md-3 col-sm-6 col-xs-12">
-                <product-item class="product-item"
-                              :options="{
-                                canAddToCart: !product.is_purchased,
-                                showPrice: !product.is_purchased,
-                                routeToProduct: !product.is_purchased,
-                                customAction: product.is_purchased,
-                                customActionLabel: 'رفتن به کلاس',
-                                customActionMessage: 'این محصول را خریده اید',
-                                product: product,
-                              }"
-                              @onCustomActionClicked="onProductClicked(product)"
-                              @click="onProductClicked(product)" />
+                   :key="product.id">
+                <div v-if="product.is_live"
+                     class="col-12">
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <product-item class="product-item"
+                                  :options="{
+                                    canAddToCart: !product.is_purchased,
+                                    showPrice: !product.is_purchased,
+                                    routeToProduct: !product.is_purchased,
+                                    customAction: product.is_purchased,
+                                    customActionLabel: 'رفتن به کلاس',
+                                    customActionMessage: 'این محصول را خریده اید',
+                                    product: product,
+                                  }"
+                                  @onCustomActionClicked="onProductClicked(product)"
+                                  @click="onProductClicked(product)" />
+                  </div>
+                </div>
+                <div v-else
+                     class="col">
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <product-item class="product-item"
+                                  :options="{
+                                    canAddToCart: !product.is_purchased,
+                                    showPrice: !product.is_purchased,
+                                    routeToProduct: !product.is_purchased,
+                                    customAction: product.is_purchased,
+                                    customActionLabel: 'رفتن به کلاس',
+                                    customActionMessage: 'این محصول را خریده اید',
+                                    product: product,
+                                  }"
+                                  @onCustomActionClicked="onProductClicked(product)"
+                                  @click="onProductClicked(product)" />
+                  </div>
+                </div>
               </div>
             </template>
             <template v-else>
@@ -152,12 +173,7 @@ export default {
     },
     getLiveConductors () {
       this.products.loading = true
-      APIGateway.product.getProductList({
-        productIds: this.localOptions.data.map(product => product.id),
-        params: {
-          length: this.localOptions.data.length
-        }
-      })
+      APIGateway.product.getLiveProducts()
         .then((products) => {
           this.products = new ProductList(products)
           this.products.loading = false
