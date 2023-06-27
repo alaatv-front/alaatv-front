ddc9f518e46289d9d6fc4ffed48f8fc9429c4d4f
kerasus <aliesmaeeli20@gmail.com>
2023-06-27 15:23:53 +0330

    add request log

--- src-ssr/server.js
+++ src-ssr/server.js
@@ -18,6 +18,7 @@ import {
   ssrRenderPreloadTag,
   ssrServeStaticContent
 } from 'quasar/wrappers'
+import audit from 'express-requests-logger'
 
 /**
  * Create your webserver and return its instance.
@@ -28,6 +29,9 @@ import {
  */
 export const create = ssrCreate((/* { ... } */) => {
   const app = express()
+  app.use(audit({
+    doubleAudit: true
+  }))
 
   // attackers can use this header to detect apps running Express
   // and then launch specifically-targeted attacks
