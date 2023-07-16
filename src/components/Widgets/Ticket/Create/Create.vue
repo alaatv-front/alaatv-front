ced38efb190d67e56e0bee914293568ea322dc8e
kerasus <aliesmaeeli20@gmail.com>
2023-07-16 14:35:35 +0330

    update dependencies

--- src/components/Widgets/Ticket/Create/Create.vue
+++ src/components/Widgets/Ticket/Create/Create.vue
@@ -169,7 +169,7 @@ export default {
       }
 
       if (priorityId) {
-        this.$refs.EntityCreate.setInputByName('priority_id', priorityId)
+        this.$refs.EntityCreate.setInputByName('priority_id', parseInt(priorityId))
       }
     },
     goBackToList() {
