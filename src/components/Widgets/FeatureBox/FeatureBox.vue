35ce51bb49c4aba3c329cdceced88373ef253506
ali_khoshnevisan <102961740+ali-khoshnevisan@users.noreply.github.com>
2023-08-08 13:59:24 +0330

    Feature box (#662)
    * abrisham2
    
    * abrisham2
    
    * ttp video player
    
    * gift card
    
    * gift card
    
    * feature box

--- src/components/Widgets/FeatureBox/FeatureBox.vue
+++ src/components/Widgets/FeatureBox/FeatureBox.vue
@@ -1,13 +1,22 @@
 <template>
-  <q-card class="feature-card">
-    <q-card-section class="feature-horizontal-section"
-                    :class="localOptions.className"
+  <q-card class="feature-card"
+          :class="localOptions.className">
+    <q-card-section :class="{'feature-horizontal-section': localOptions.horizontal, 'feature-vertical-section': !localOptions.horizontal}"
                     :horizontal="localOptions.horizontal">
-      <img class="feature-image"
-           :src="localOptions.image">
+      <!--      <image-widget v-if="localOptions.image"-->
+      <q-img v-if="localOptions.image"
+             :src="localOptions.image"
+             :width="localOptions.imageWidth"
+             :height="localOptions.imageHeight" />
       <q-card-section>
-        <div class="feature-title ellipsis">{{ localOptions.title }}</div>
-        <div class="feature-description">
+        <text-widget v-if="localOptions.titleOptions.text"
+                     :options="localOptions.titleOptions" />
+        <div v-else
+             class="feature-title ellipsis">{{ localOptions.title }}</div>
+        <text-widget v-if="localOptions.descriptionOptions.text"
+                     :options="localOptions.descriptionOptions" />
+        <div v-else
+             class="feature-description">
           {{ localOptions.description }}
         </div>
       </q-card-section>
@@ -16,18 +25,98 @@
 </template>
 
 <script>
-import { mixinWidget, mixinPrefetchServerData } from 'src/mixin/Mixins.js'
+import { mixinWidget } from 'src/mixin/Mixins.js'
+import textWidget from 'components/Widgets/TextWidget/TextWidget.vue'
+// import imageWidget from 'components/Widgets/ImageWidget/ImageWidget.vue'
 
 export default {
   name: 'FeatureBox',
-  mixins: [mixinPrefetchServerData, mixinWidget],
+  components: {
+    textWidget
+    // imageWidget
+  },
+  mixins: [mixinWidget],
   data() {
     return {
       defaultOptions: {
-        title: null,
-        description: null,
+        title: '',
+        titleOptions: {
+          text: null,
+          color: null,
+          fontFamily: null,
+          xs: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          sm: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          md: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          lg: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          xl: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          }
+        },
+        description: '',
+        descriptionOptions: {
+          text: null,
+          fontFamily: null,
+          color: null,
+          xs: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          sm: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          md: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          lg: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          xl: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          }
+        },
         image: null,
-        horizontal: true
+        imageWidth: '100px',
+        imageHeight: '100px',
+        horizontal: true,
+        theme: 'theme1',
+        borderStyle: {}
       }
     }
   }
@@ -35,41 +124,172 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border: v-bind('localOptions.borderStyle.borderCssString');
+$borderRadius: v-bind('localOptions.borderStyle.borderRadiusCssString');
 .feature-card {
   width: 100%;
   max-width: 100%;
   box-shadow: none;
+  -webkit-border-radius: $borderRadius;
+  -moz-border-radius: $borderRadius;
+  border: $border;
   .feature-horizontal-section {
 
-    .feature-image {
-      width: 100px;
-      height: 100px;
-    }
-
     .feature-title {
-      font-weight: 700;
-      font-size: 20px;
-      line-height: 35px;
+      line-height: v-bind('localOptions.titleOptions.xl.lineHeight');
+      color: v-bind('localOptions.titleOptions.color');
+      font-size: v-bind('localOptions.titleOptions.xl.fontSize');
+      font-weight: v-bind('localOptions.titleOptions.xl.fontWeight');
+      font-family: v-bind('localOptions.titleOptions.fontFamily');
+      font-style: v-bind('localOptions.titleOptions.xl.fontStyle');
+
+      @media screen and (max-width: 1920px) {
+        font-size: v-bind('localOptions.titleOptions.lg.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.lg.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.lg.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.lg.lineHeight');
+      }
+
+      @media screen and (max-width: 1440px) {
+        font-size: v-bind('localOptions.titleOptions.md.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.md.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.md.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.md.lineHeight');
+      }
+
+      @media screen and (max-width: 1024px) {
+        font-size: v-bind('localOptions.titleOptions.sm.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.sm.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.sm.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.sm.lineHeight');
+      }
 
       @media screen and (max-width: 600px) {
-        font-weight: 600;
-        font-size: 18px;
-        line-height: 26px;
+        font-size: v-bind('localOptions.titleOptions.xs.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.xs.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.xs.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.xs.lineHeight');
       }
     }
 
     .feature-description {
       max-width: 300px;
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 31px;
+      line-height: v-bind('localOptions.descriptionOptions.xl.lineHeight');
+      color: v-bind('localOptions.descriptionOptions.color');
+      font-size: v-bind('localOptions.descriptionOptions.xl.fontSize');
+      font-weight: v-bind('localOptions.descriptionOptions.xl.fontWeight');
+      font-family: v-bind('localOptions.descriptionOptions.fontFamily');
+      font-style: v-bind('localOptions.descriptionOptions.xl.fontStyle');
+
+      @media screen and (max-width: 1920px) {
+        font-size: v-bind('localOptions.descriptionOptions.lg.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.lg.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.lg.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.lg.lineHeight');
+      }
+
+      @media screen and (max-width: 1440px) {
+        font-size: v-bind('localOptions.descriptionOptions.md.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.md.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.md.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.md.lineHeight');
+      }
+
+      @media screen and (max-width: 1024px) {
+        font-size: v-bind('localOptions.descriptionOptions.sm.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.sm.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.sm.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.sm.lineHeight');
+      }
 
       @media screen and (max-width: 600px) {
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 22px;
+        font-size: v-bind('localOptions.descriptionOptions.xs.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.xs.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.xs.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.xs.lineHeight');
       }
     }
   }
+
+  .feature-vertical-section {
+    text-align-last: center;
+
+    .feature-title {
+      text-align: center;
+      line-height: v-bind('localOptions.titleOptions.xl.lineHeight');
+      color: v-bind('localOptions.titleOptions.color');
+      font-size: v-bind('localOptions.titleOptions.xl.fontSize');
+      font-weight: v-bind('localOptions.titleOptions.xl.fontWeight');
+      font-family: v-bind('localOptions.titleOptions.fontFamily');
+      font-style: v-bind('localOptions.titleOptions.xl.fontStyle');
+
+      @media screen and (max-width: 1920px) {
+        font-size: v-bind('localOptions.titleOptions.lg.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.lg.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.lg.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.lg.lineHeight');
+      }
+
+      @media screen and (max-width: 1440px) {
+        font-size: v-bind('localOptions.titleOptions.md.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.md.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.md.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.md.lineHeight');
+      }
+
+      @media screen and (max-width: 1024px) {
+        font-size: v-bind('localOptions.titleOptions.sm.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.sm.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.sm.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.sm.lineHeight');
+      }
+
+      @media screen and (max-width: 600px) {
+        font-size: v-bind('localOptions.titleOptions.xs.fontSize');
+        font-weight: v-bind('localOptions.titleOptions.xs.fontWeight');
+        font-style: v-bind('localOptions.titleOptions.xs.fontStyle');
+        line-height: v-bind('localOptions.titleOptions.xs.lineHeight');
+      }
+    }
+
+    .feature-description {
+      text-align: center;
+      line-height: v-bind('localOptions.descriptionOptions.xl.lineHeight');
+      color: v-bind('localOptions.descriptionOptions.color');
+      font-size: v-bind('localOptions.descriptionOptions.xl.fontSize');
+      font-weight: v-bind('localOptions.descriptionOptions.xl.fontWeight');
+      font-family: v-bind('localOptions.descriptionOptions.fontFamily');
+      font-style: v-bind('localOptions.descriptionOptions.xl.fontStyle');
+
+      @media screen and (max-width: 1920px) {
+        font-size: v-bind('localOptions.descriptionOptions.lg.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.lg.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.lg.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.lg.lineHeight');
+      }
+
+      @media screen and (max-width: 1440px) {
+        font-size: v-bind('localOptions.descriptionOptions.md.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.md.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.md.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.md.lineHeight');
+      }
+
+      @media screen and (max-width: 1024px) {
+        font-size: v-bind('localOptions.descriptionOptions.sm.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.sm.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.sm.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.sm.lineHeight');
+      }
+
+      @media screen and (max-width: 600px) {
+        font-size: v-bind('localOptions.descriptionOptions.xs.fontSize');
+        font-weight: v-bind('localOptions.descriptionOptions.xs.fontWeight');
+        font-style: v-bind('localOptions.descriptionOptions.xs.fontStyle');
+        line-height: v-bind('localOptions.descriptionOptions.xs.lineHeight');
+      }
+    }
+  }
+
 }
 </style>
--- src/components/Widgets/FeatureBox/OptionPanel.vue
+++ src/components/Widgets/FeatureBox/OptionPanel.vue
@@ -1,21 +1,44 @@
 <template>
-  <option-panel-tabs v-model:options="localOptions">
+  <option-panel-tabs v-model:options="localOptions"
+                     :show-border-style-tab="true">
     <template #main-tab>
-      <div class="option-panel-container">
-        <div class="input-container q-py-md">
+      <div class="option-panel-container row q-col-gutter-md">
+        <div class="input-container q-my-md col-md-12">
           <div class="outsideLabel">image</div>
           <q-input v-model="localOptions.image"
                    label="image" />
         </div>
-        <div class="input-container q-py-md">
-          <div class="outsideLabel">title</div>
-          <q-input v-model="localOptions.title"
-                   label="image" />
+        <div class="input-container col-md-3">
+          <div class="outsideLabel">width</div>
+          <q-input v-model="localOptions.imageWidth"
+                   label="width" />
         </div>
-        <div class="input-container q-py-md">
-          <div class="outsideLabel">description</div>
-          <q-input v-model="localOptions.description"
-                   label="image" />
+        <div class="input-container col-md-3">
+          <div class="outsideLabel">height</div>
+          <q-input v-model="localOptions.imageHeight"
+                   label="height" />
+        </div>
+        <div class="col-md-2">
+          <div class="outsideLabel">horizontal</div>
+          <q-checkbox v-model="localOptions.horizontal" />
+        </div>
+        <div class="input-container col-md-12">
+          <q-expansion-item label="title">
+            <q-card>
+              <q-card-section>
+                <text-option-panel v-model:options="localOptions.titleOptions" />
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </div>
+        <div class="input-container col-md-12">
+          <q-expansion-item label="description">
+            <q-card>
+              <q-card-section>
+                <text-option-panel v-model:options="localOptions.descriptionOptions" />
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
         </div>
       </div>
     </template>
@@ -23,27 +46,97 @@
 </template>
 <script>
 import { defineComponent } from 'vue'
-import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
 import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+import { PageBuilderOptionPanel } from 'src/mixin/Mixins.js'
+import textOptionPanel from 'components/Widgets/TextWidget/OptionPanel.vue'
 
 export default defineComponent({
   name: 'OptionPanel',
-  components: { OptionPanelTabs },
-  mixins: [mixinOptionPanel],
-  props: {
-    options: {
-      type: Object,
-      default() {
-        return {}
-      }
-    }
+  components: {
+    OptionPanelTabs,
+    textOptionPanel
   },
-  watch: {
-    localOptions: {
-      handler(newVal) {
-        this.$emit('update:options', newVal)
-      },
-      deep: true
+  mixins: [PageBuilderOptionPanel],
+  data() {
+    return {
+      themeOptions: ['theme1', 'theme2'],
+      defaultOptions: {
+        title: '',
+        titleOptions: {
+          text: null,
+          color: null,
+          fontFamily: null,
+          xs: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          sm: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          md: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          lg: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          xl: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          }
+        },
+        description: '',
+        descriptionOptions: {
+          fontFamily: null,
+          color: null,
+          xs: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          sm: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          md: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          lg: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          },
+          xl: {
+            fontSize: null,
+            fontWeight: null,
+            fontStyle: null,
+            lineHeight: null
+          }
+        },
+        image: null,
+        horizontal: true,
+        theme: 'theme1',
+        borderStyle: {}
+      }
     }
   }
 })
