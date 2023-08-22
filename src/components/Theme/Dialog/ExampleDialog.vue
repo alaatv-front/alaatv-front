8b5b8970e5938204a6497ed05fe77b12d2e79627
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-08-22 14:27:43 +0330

    theme

--- src/components/Theme/Dialog/ExampleDialog.vue
+++ src/components/Theme/Dialog/ExampleDialog.vue
@@ -1,47 +1,54 @@
 <template>
-  <div>
-    dialog
-    <div v-for="(item, index) in sizes"
-         :key="index">
-      <q-btn color="primary"
-             @click="dialogValue[item.size] = true">Open Dialog {{item.size}}</q-btn>
-      <q-dialog v-model="dialogValue[item.size]">
-        <q-card :class="item.cardClass">
-          <q-card-section class="dialog-header">
-            <q-avatar :class="'avatar-'+item.size"
-                      class="dialog-avatar"
-                      color="positive"
-                      text-color="white"
-                      icon="check" />
-            <h5 class="dialog-header-title">عنوان متن</h5>
-            <q-btn class="dialog-close-icon"
-                   icon="close"
-                   flat
-                   unelevated />
-          </q-card-section>
-          <q-separator class="grey1" />
-          <q-card-section class="dialog-body">
-            <h5>آیا می‌خواهید لورم ایپسوم شوید؟</h5>
-          </q-card-section>
-          <q-card-section class="dialog-action">
-            <q-btn class="q-btn-md dialog-action-default-btn"
-                   flat>
-              متن دکمه
-            </q-btn>
-            <div class="dialog-action-btn-box">
-              <q-btn class="q-btn-md q-mr-sm"
-                     color="secondary"
-                     outline>
-                انصراف
-              </q-btn>
-              <q-btn class="q-btn-md"
-                     color="positive">
-                تایید
-              </q-btn>
-            </div>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
+  <div class="row justify-center">
+    <div class="col-md-9">
+      <h2>
+        dialog
+      </h2>
+      <div class="row q-col-gutter-md">
+        <q-btn color="primary"
+               @click="dialog = true">Open Dialog</q-btn>
+        <q-dialog v-model="dialog">
+          <q-card>
+            <q-card-section class="dialog-header-section">
+              <div class="row items-center justify-between">
+                <div>
+                  <q-icon class="dialog-avatar"
+                          color="positive"
+                          name="check" />
+                  عنوان متن
+                </div>
+                <q-btn flat
+                       icon="close"
+                       color="grey-6"
+                       @click="dialog = false" />
+              </div>
+            </q-card-section>
+            <q-separator class="grey1" />
+            <q-card-section class="dialog-body-section">
+              <div class="body2">آیا می‌خواهید لورم ایپسوم شوید؟</div>
+            </q-card-section>
+            <q-card-section class="dialog-action-section">
+              <div class="row items-center justify-between">
+                <q-btn class="q-btn-md dialog-action-default-btn"
+                       flat>
+                  متن دکمه
+                </q-btn>
+                <div class="dialog-action-btn-box">
+                  <q-btn class="q-btn-md q-mr-sm"
+                         color="secondary"
+                         outline>
+                    انصراف
+                  </q-btn>
+                  <q-btn class="q-btn-md"
+                         color="positive">
+                    تایید
+                  </q-btn>
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+      </div>
     </div>
   </div>
 </template>
@@ -51,35 +58,7 @@ export default {
   name: 'ExampleDialog',
   data() {
     return {
-      dialogValue: {
-        xs: false,
-        sm: false,
-        md: false,
-        lg: false,
-        xl: false
-      },
-      sizes: [
-        {
-          size: 'xs',
-          cardClass: 'dialog-xs'
-        },
-        {
-          size: 'sm',
-          cardClass: 'dialog-sm'
-        },
-        {
-          size: 'md',
-          cardClass: 'dialog-md'
-        },
-        {
-          size: 'lg',
-          cardClass: 'dialog-lg'
-        },
-        {
-          size: 'xl',
-          cardClass: 'dialog-xl'
-        }
-      ]
+      dialog: false
     }
   }
 }
