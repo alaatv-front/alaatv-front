b5c84b1cfd4ec41e31bc5b55ac04a0da04d9dea6
kerasus <aliesmaeeli20@gmail.com>
2023-08-23 15:48:02 +0330

    verify mobile number in profile

---
+++ src/components/Widgets/User/UserMobileVerification/OptionPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <option-panel-tabs v-model:options="localOptions">
+    <template #main-tab />
+  </option-panel-tabs>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
+import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+
+export default defineComponent({
+  name: 'OptionPanel',
+  components: { OptionPanelTabs },
+  mixins: [mixinOptionPanel],
+  data() {
+    return {
+      accountInfo: true,
+      personalInfo: true,
+      educationalInfo: true,
+      contactInfo: true,
+      defaultOptions: {
+        accountInfo: true,
+        personalInfo: true,
+        educationalInfo: true,
+        contactInfo: true,
+        className: '',
+        height: 'auto',
+        boxed: false,
+        boxedWidth: 1200,
+        style: {}
+      }
+    }
+  }
+})
+</script>
---
+++ src/components/Widgets/User/UserMobileVerification/UserMobileVerification.vue
@@ -0,0 +1,175 @@
+<template>
+  <div :class="options.className"
+       :style="options.style">
+    <q-tab-panels v-model="panel"
+                  animated
+                  class="UserMobileVerification">
+      <q-tab-panel name="inputMobile">
+        تایید شماره همراه:
+        <q-input v-model="user.mobile"
+                 disable
+                 class="q-mb-md" />
+        <q-btn color="primary"
+               :loading="user.loading"
+               class="float-right"
+               @click="sendVerifyCode">
+          ارسال کد تایید
+        </q-btn>
+      </q-tab-panel>
+
+      <q-tab-panel name="verifyCode">
+        لطفا کد تایید ارسال شده به شماره
+        {{ user.mobile }}
+        را وارد کنید
+        <q-input v-model="verifyCode"
+                 class="q-mb-md" />
+        <div class="resend-timer-message q-mb-md">
+          <template v-if="!timerEnded">
+            <span class="resend-timer-message-value">
+              <timer ref="timer"
+                     :time="120"
+                     @stop="onStopTimer" />
+            </span>
+            تا درخواست مجدد
+          </template>
+          <template v-else>
+            <q-btn color="primary"
+                   class="float-left"
+                   :loading="user.loading"
+                   @click="restartTimer">
+              ارسال مجدد کد تایید
+            </q-btn>
+          </template>
+        </div>
+
+        <q-btn color="primary"
+               :loading="user.loading"
+               class="float-right"
+               @click="verifyMobile">
+          تایید شماره همراه
+        </q-btn>
+      </q-tab-panel>
+
+      <q-tab-panel name="verified">
+        <div class="verified-panel">
+          <div class="image-section">
+            <lazy-img src="https://nodes.alaatv.com/aaa/landing/Soalaa/States/thankyou_page.png"
+                      width="50"
+                      height="50" />
+          </div>
+          <div>
+            شماره شما تایید شده است.
+          </div>
+          <div>
+            ({{ user.mobile }})
+          </div>
+        </div>
+      </q-tab-panel>
+    </q-tab-panels>
+  </div>
+</template>
+
+<script>
+import { User } from 'src/models/User.js'
+import Timer from './components/Timer.vue'
+import LazyImg from 'src/components/lazyImg.vue'
+import { mixinWidget } from 'src/mixin/Mixins.js'
+import { APIGateway } from 'src/api/APIGateway.js'
+
+export default {
+  name: 'UserMobileVerification',
+  components: { LazyImg, Timer },
+  mixins: [mixinWidget],
+  data () {
+    return {
+      user: new User(),
+      panel: 'inputMobile',
+      verifyCode: null,
+      timerEnded: false,
+      sendOtpLoading: false,
+      mobile: null
+    }
+  },
+  mounted () {
+    this.loadAuthData()
+    const isMobileIsVerified = this.isMobileIsVerified()
+    if (isMobileIsVerified) {
+      this.panel = 'verified'
+    }
+  },
+  methods: {
+    loadAuthData () {
+      this.user = this.$store.getters['Auth/user']
+    },
+    isMobileIsVerified () {
+      return !!this.user.mobile_verified_at
+    },
+    sendVerifyCode () {
+      this.user.loading = true
+      APIGateway.user.mobileResend()
+        .then(({ code, message }) => {
+          this.user.loading = false
+          if (code) {
+            this.verifyCode = code
+          }
+          this.panel = 'verifyCode'
+          this.timerEnded = false
+          this.$nextTick(() => {
+            this.$refs.timer.startTimer()
+          })
+          this.$q.notify({
+            message,
+            type: 'positive'
+          })
+        })
+        .catch(() => {
+          this.user.loading = false
+        })
+    },
+    verifyMobile () {
+      this.user.loading = true
+      APIGateway.user.mobileVerify({ code: this.verifyCode })
+        .then(({ code, message }) => {
+          this.user.loading = false
+          if (code) {
+            this.verifyCode = code
+          }
+          this.panel = 'verified'
+          this.user.mobile_verified_at = true
+          this.$store.dispatch('Auth/updateUser')
+          this.$q.notify({
+            message,
+            type: 'positive'
+          })
+        })
+        .catch(() => {
+          this.user.loading = false
+        })
+    },
+    onStopTimer () {
+      this.timerEnded = true
+    },
+    restartTimer () {
+      this.sendVerifyCode()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.UserMobileVerification {
+  box-shadow: -2px -4px 10px rgba(255, 255, 255, 0.6), 2px 4px 10px rgba(112, 108, 162, 0.05);
+  border-radius: 20px;
+  background: #fff;
+  padding: 16px;
+  .verified-panel {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    .image-section {
+      width: 50px;
+    }
+  }
+}
+</style>
