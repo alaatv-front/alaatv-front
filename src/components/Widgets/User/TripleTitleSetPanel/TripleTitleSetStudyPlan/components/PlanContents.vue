d0dc51c381f58b004f0d2ab8526bbefb3c953472
ali_khoshnevisan <102961740+ali-khoshnevisan@users.noreply.github.com>
2023-08-28 16:12:45 +0330

    study plan (#692)
    * abrisham2
    
    * abrisham2
    
    * full calendar
    
    * full calendar
    
    * full calendar
    
    * full calendar
    
    * ttp
    
    * ttp
    
    * study plan
    
    * study plan
    
    * inputs col
    
    * ttp video player
    
    * gift card
    
    * gift card
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * header menu
    
    * study plan
    
    * study plan
    
    * study plan footer
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * national card
    
    * national card
    
    * national card
    
    * national card
    
    * gift card image
    
    * gift card image
    
    * gift card image
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    * study plan
    
    ---------
    
    Co-authored-by: kerasus <aliesmaeeli20@gmail.com>

---
+++ src/components/Widgets/User/TripleTitleSetPanel/TripleTitleSetStudyPlan/components/PlanContents.vue
@@ -0,0 +1,155 @@
+<template>
+  <div v-for="content in plan.contents.list"
+       :key="content.id"
+       class="col plan-item-box q-mx-sm">
+    <div class="plan-item-header" />
+    <div class="plan-item-info">
+      <div class="item-title ellipsis">{{ content.title }}</div>
+      <div class="item-plan ellipsis">
+        <q-icon name="import_contacts"
+                size="18px"
+                class="q-mr-sm" />
+        {{ content?.title || 'ویدیو ندارد' }}
+      </div>
+    </div>
+    <q-separator />
+    <div class="plan-item-footer">
+      <div class="footer-text">
+        مشاهده
+      </div>
+      <div class="footer-action">
+        <q-btn color="primary"
+               round
+               unelevated
+               :disable="!content.id"
+               size="11px"
+               icon="play_arrow"
+               :to="{name:'UserPanel.Asset.TripleTitleSet.Content', params:{ productId: plan.product?.id, setId: content.set?.id, contentId: content?.id}}" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Plan } from 'src/models/Plan'
+
+export default {
+  name: 'PlanContents',
+  props: {
+    plan: {
+      type: Plan
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.plan-item-box{
+  width: 390px;
+  height: 158px;
+  border-radius: 12px;
+  background: #F5F7FA;
+  padding: 16px 20px 20px 20px;
+
+  @media only screen and (max-width: 600px) {
+    width: 230px;
+    height: 196px;
+  }
+
+  .plan-item-header {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    .plan-time {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 3px 6px 4px 6px;
+      border-radius: 8px;
+      background: #ECEFF1;
+      &.now {
+        background: #FFD54F;
+      }
+    }
+  }
+
+  .plan-item-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    margin-bottom: 20px;
+
+    @media only screen and (max-width: 600px) {
+      margin-bottom: 20px;
+    }
+
+    .item-title {
+      max-width: 100%;
+      color:#424242;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      letter-spacing: -0.32px;
+      margin-bottom: 10px;
+
+      @media only screen and (max-width: 600px) {
+        margin: 16px 0 8px;
+      }
+    }
+
+    .item-plan {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      max-width: 100%;
+      color: #757575;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: -0.24px;
+    }
+  }
+
+  .plan-item-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 16px;
+
+    @media only screen and (max-width: 600px) {
+      margin-top: 16px;
+    }
+
+    .footer-text {
+      color: #424242;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      letter-spacing: -0.32px;
+
+      &.watched-text {
+        color: #26A69A;
+      }
+    }
+
+    .footer-action {
+
+      &:deep(.q-btn) {
+        border-radius: 50%;
+      }
+      .future{
+        color: #616161;
+        background: #ECEFF1;
+      }
+      .watched {
+        color: #26A69A;
+      }
+    }
+  }
+}
+</style>
