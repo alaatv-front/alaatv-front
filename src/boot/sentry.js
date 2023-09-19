40d31566a94e311f72f634b36c2970db7bc5eae9
kerasus <aliesmaeeli20@gmail.com>
2023-09-19 15:16:55 +0330

    create boot

---
+++ src/boot/sentry.js
@@ -0,0 +1,28 @@
+import * as Sentry from '@sentry/vue'
+import { boot } from 'quasar/wrappers'
+
+export default boot(({ app, router }) => {
+  Sentry.init({
+    app,
+    dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+      }),
+      new Sentry.Replay()
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0
+  })
+})
