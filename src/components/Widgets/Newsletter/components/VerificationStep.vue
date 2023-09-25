478dd8161faba5f77fbe7185e681d3b76905e663
kerasus <aliesmaeeli20@gmail.com>
2023-09-25 18:18:39 +0330

    otp for UserMobileVerification

--- src/components/Widgets/Newsletter/components/VerificationStep.vue
+++ src/components/Widgets/Newsletter/components/VerificationStep.vue
@@ -86,7 +86,9 @@ export default {
     }
   },
   mounted () {
-    this.loadOTPCredential()
+    this.$nextTick(() => {
+      this.loadOTPCredential()
+    })
   },
   methods: {
     loadOTPCredential () {
