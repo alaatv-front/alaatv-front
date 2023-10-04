0486424b306c464c6b6c6f33a6dc4fcf7232119f
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-10-04 19:44:44 +0330

    gift card create

---
+++ src/pages/Admin/GiftCard/Create.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page-builder v-model:sections="sections"
+                  v-model:options="pageConfig"
+                  :editable="pageBuilderEditable" />
+</template>
+
+<script>
+
+export default {
+  name: 'CreateGiftCard',
+  data() {
+    return {
+      pageConfig: {},
+      sections: [
+        {
+          data: {
+            rows: [
+              {
+                cols: [
+                  {
+                    widgets: [
+                      {
+                        name: 'AdminGiftCardCreate'
+                      }
+                    ]
+                  }
+                ],
+                options: {
+                  boxed: true,
+                  boxedWidth: 1362
+                }
+              }
+            ]
+          },
+          options: {
+            // fullHeight: true
+          }
+        }
+      ]
+    }
+  },
+  computed: {
+    pageBuilderEditable () {
+      return this.$store.getters['AppLayout/pageBuilderEditable']
+    }
+  }
+}
+</script>
