f83c4f852cec39d6e4e3fbcd000e66927440f002
sohrab <sohrab.a.fard@gmail.com>
2023-10-16 00:37:15 +0330

    limit 200 request only in browser, unlimit request in pwa mode

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -6,6 +6,7 @@
  * quasar.config.js > pwa > workboxMode is set to "injectManifest"
  */
 
+// Import necessary workbox libraries
 import { clientsClaim } from 'workbox-core'
 import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
 import { registerRoute, NavigationRoute } from 'workbox-routing'
@@ -13,25 +14,26 @@ import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies'
 import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { ExpirationPlugin } from 'workbox-expiration'
 
+// Ensure the service worker takes control of the page as soon as possible.
 self.skipWaiting()
 clientsClaim()
 
-// Placeholder variables for environment-specific values
+// Define placeholders for environment variables
 const ASSET_SERVE = '__ASSET_SERVE__'
 const NODES_SERVER_URL_SSL = '__NODES_SERVER_URL_SSL__'
 const MODE = '__MODE__'
 const PROD = '__PROD__'
 const PWA_FALLBACK_HTML = '__PWA_FALLBACK_HTML__'
 
-// Extract the origin from NODES_SERVER_URL_SSL
+// Extract the origin from NODES_SERVER_URL_SSL for asset matching
 const ASSET_ORIGIN = new URL(NODES_SERVER_URL_SSL).origin
 
-// Define fallbacks for different asset types
+// Define fallback URLs for different asset types
 const FALLBACK_IMAGE = '/path-to-default-image.jpg'
 const FALLBACK_SCRIPT = '/path-to-default-script.js'
 const FALLBACK_STYLE = '/path-to-default-style.css'
 
-// Sort the manifest to prioritize JS, then CSS, then fonts, then others
+// Sort the manifest to prioritize prefetching of JS, then CSS, then fonts, then others
 const sortedManifest = self.__WB_MANIFEST.sort((a, b) => {
   const getFilePriority = (url) => {
     if (url.endsWith('.js')) return 1
@@ -39,35 +41,36 @@ const sortedManifest = self.__WB_MANIFEST.sort((a, b) => {
     if (url.endsWith('.woff') || url.endsWith('.woff2') || url.endsWith('.ttf')) return 3
     return 4
   }
-
   return getFilePriority(a.url) - getFilePriority(b.url)
 })
 
-// Limit the number of resources prefetched by the service worker
-const MAX_PREFETCH = 200
-const limitedResources = sortedManifest.slice(0, MAX_PREFETCH)
+// Determine if the app is running in standalone (PWA) mode or in a browser
+const isStandalone = (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone || document.referrer.includes('android-app://'))
 
-// Adjust manifest based on asset serving strategy
+// If in standalone mode, prefetch all resources; otherwise, limit to the first 200
+const resourcesToPrefetch = isStandalone ? sortedManifest : sortedManifest.slice(0, 200)
+
+// Adjust the manifest based on the asset serving mode (remote or local)
 if (ASSET_SERVE === 'remote') {
   const prefix = NODES_SERVER_URL_SSL
-  const adjustedManifest = limitedResources.map(entry => {
+  const adjustedManifest = resourcesToPrefetch.map(entry => {
     return { ...entry, url: `${prefix}${entry.url}` }
   })
   precacheAndRoute(adjustedManifest)
 } else {
-  precacheAndRoute(limitedResources)
+  precacheAndRoute(resourcesToPrefetch)
 }
 
 // Clean up outdated caches
 cleanupOutdatedCaches()
 
-// Strategy to serve stale content while revalidating in the background for scripts and styles
+// Define a strategy to serve stale content while revalidating in the background
 registerRoute(
   ({ request }) => request.destination === 'script' || request.destination === 'style',
   new StaleWhileRevalidate()
 )
 
-// Use Cache First, then Network strategy for assets
+// Define a Cache First, then Network strategy for assets
 registerRoute(
   ({ url }) => url.origin === ASSET_ORIGIN,
   new CacheFirst({
@@ -77,46 +80,21 @@ registerRoute(
         fetchDidFail: async ({ originalRequest }) => {
           // This callback is triggered whenever a network request fails
           const cache = await caches.open('alaatv-assets')
-          let response = await cache.match(originalRequest)
-          if (!response) {
+          const response = await fetch(originalRequest)
+          if (response && response.status === 200) {
+            await cache.put(originalRequest, response.clone())
+            return response
+          } else {
             // Handle different fallbacks based on the request type
             switch (originalRequest.destination) {
               case 'image':
-                response = await cache.match(FALLBACK_IMAGE)
-                break
+                return caches.match(FALLBACK_IMAGE)
               case 'script':
-                response = await cache.match(FALLBACK_SCRIPT)
-                break
+                return caches.match(FALLBACK_SCRIPT)
               case 'style':
-                response = await cache.match(FALLBACK_STYLE)
-                break
+                return caches.match(FALLBACK_STYLE)
             }
           }
-          // If the fallback is also not in the cache, provide a generic response
-          if (!response) {
-            switch (originalRequest.destination) {
-              case 'image':
-                // Return a 1x1 transparent pixel as a fallback for images
-                response = new Response(
-                  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
-                  { headers: { 'Content-Type': 'image/gif' } }
-                )
-                break
-              case 'script':
-                // Return an empty script
-                response = new Response('', { headers: { 'Content-Type': 'text/javascript' } })
-                break
-              case 'style':
-                // Return an empty stylesheet
-                response = new Response('', { headers: { 'Content-Type': 'text/css' } })
-                break
-              default:
-                // Return a generic message for other types
-                response = new Response('Resource not available', { status: 404 })
-                break
-            }
-          }
-          return response
         }
       },
       // Ensure that only cacheable responses are cached
