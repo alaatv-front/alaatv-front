2f9ee550872a2b875eaeba7ea2f03df57494abb6
kerasus <aliesmaeeli20@gmail.com>
2023-10-15 17:51:53 +0330

    update quasar config publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -83,8 +83,8 @@ module.exports = configure(function (ctx) {
       sourcemap: false,
       gzip: true,
       analyze: false,
-      publicPath: (process.env.ASSET_SERVE === 'remote') ? (process.env.NODES_SERVER_URL_SSL || '/') : '/',
-      // publicPath: '/',
+      // publicPath: (process.env.ASSET_SERVE === 'remote') ? (process.env.NODES_SERVER_URL_SSL || '/') : '/',
+      publicPath: '/',
       env: process.env,
       extendViteConf(viteConf, { isServer, isClient }) {
         // Set the base URL based on the environment
