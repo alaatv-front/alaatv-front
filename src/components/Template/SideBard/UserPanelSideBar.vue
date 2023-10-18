27378a477237bd202bb874adc26a4a935e23fa44
kerasus <aliesmaeeli20@gmail.com>
2023-10-18 18:19:00 +0330

    init

--- src/components/Template/SideBard/UserPanelSideBar.vue
+++ src/components/Template/SideBard/UserPanelSideBar.vue
@@ -1,22 +1,21 @@
 <template>
-  <profile-menu class="UserPanelSideBar" />
+  <!--  <profile-menu class="UserPanelSideBar" />-->
+  <div class="UserPanelSideBar">
+    new profile
+  </div>
 </template>
 
 <script>
 
-import ProfileMenu from 'components/Widgets/User/ProfileMenu/ProfileMenu.vue'
+// import ProfileMenu from 'components/Widgets/User/ProfileMenu/ProfileMenu.vue'
 export default {
-  name: 'UserPanelSideBar',
-  components: { ProfileMenu }
+  name: 'UserPanelSideBar'
+  // components: { ProfileMenu }
 }
 </script>
 
 <style scoped lang="scss">
 .UserPanelSideBar {
-  background: white;
-  height: 100%;
-  :deep(.custom-card){
-    box-shadow: none !important;
-  }
+
 }
 </style>
