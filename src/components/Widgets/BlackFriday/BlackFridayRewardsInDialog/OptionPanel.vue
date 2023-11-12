ea3631a36a80534f7166486970132a1aca0f56d3
Ayhan <ayhan.samimi@gmail.com>
2023-11-12 15:16:48 +0330

    fix: black Friday Auth handling & rewards dialog option panel (#821)

---
+++ src/components/Widgets/BlackFriday/BlackFridayRewardsInDialog/OptionPanel.vue
@@ -0,0 +1,45 @@
+<template>
+  <option-panel-tabs v-model:options="localOptions"
+                     :show-responsive-spacing="true"
+                     :show-box-shadows-tab="true"
+                     :show-responsive-show="true">
+    <template #main-tab>
+      <div class="option-panel-container q-py-md">
+        <div class="row q-col-gutter-sm">
+          <div class="input-container col-md-10">
+            <q-input v-model="localOptions.eventName"
+                     label="Event Name" />
+          </div>
+        </div>
+      </div>
+    </template>
+  </option-panel-tabs>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import { mixinOptionPanel } from 'quasar-ui-q-page-builder'
+import OptionPanelTabs from 'quasar-ui-q-page-builder/src/components/OptionPanelComponents/OptionPanelTabs.vue'
+
+export default defineComponent({
+  name: 'OptionPanel',
+  components: {
+    OptionPanelTabs
+  },
+  mixins: [mixinOptionPanel],
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      defaultOptions: {
+        eventName: null
+      }
+    }
+  }
+})
+</script>
