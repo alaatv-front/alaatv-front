1e0324c02cd5867515f68951ca874163981e762f
ali-khoshnevisan <ali.kh.3437@gmail.com>
2023-11-21 13:19:54 +0330

    feat: content list dialog

--- src/components/Widgets/User/TripleTitleSetPanel/TripleTitleSetStudyPlan/components/Contents.vue
+++ src/components/Widgets/User/TripleTitleSetPanel/TripleTitleSetStudyPlan/components/Contents.vue
@@ -1,40 +1,44 @@
 <template>
-  <q-inner-loading v-if="loading"
-                   :showing="loading" />
-  <div v-else>
-    <div>
-      کد جلسات
-    </div>
-    <div class="row">
-      <q-input v-model="content"
-               filled
-               class="col-10"
-               @keyup.enter="onKeyUp($event)" />
-      <q-btn class="col q-ml-md"
-             color="primary"
-             text-color="grey-9"
-             icon="add"
-             @click="showContentDemo" />
-      <div v-for="(content, index) in localContents"
-           :key="index"
-           class="col-12 row q-mt-sm">
-        <q-icon class="col-1"
-                name="isax:play-circle" />
-        <span class="col-10">{{content.id}} - {{content.title}}</span>
-        <q-icon name="isax:trash"
-                color="red"
-                class="col-1 cursor-pointer"
-                @click="removeContentDemo(index)" />
-      </div>
-    </div>
-  </div>
+  <triple-title-set-content-selection v-model:selectedContentList="localContents" />
+  <!--  <q-inner-loading v-if="loading"-->
+<!--                   :showing="loading" />-->
+<!--  <div v-else>-->
+<!--    <div>-->
+<!--      کد جلسات-->
+<!--    </div>-->
+<!--    <div class="row">-->
+<!--      <q-input v-model="content"-->
+<!--               filled-->
+<!--               class="col-10"-->
+<!--               @keyup.enter="onKeyUp($event)" />-->
+<!--      <q-btn class="col q-ml-md"-->
+<!--             color="primary"-->
+<!--             text-color="grey-9"-->
+<!--             icon="add"-->
+<!--             @click="showContentDemo" />-->
+<!--      <div v-for="(content, index) in localContents"-->
+<!--           :key="index"-->
+<!--           class="col-12 row q-mt-sm">-->
+<!--        <q-icon class="col-1"-->
+<!--                name="isax:play-circle" />-->
+<!--        <span class="col-10">{{content.id}} - {{content.title}}</span>-->
+<!--        <q-icon name="isax:trash"-->
+<!--                color="red"-->
+<!--                class="col-1 cursor-pointer"-->
+<!--                @click="removeContentDemo(index)" />-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script>
 import { APIGateway } from 'src/api/APIGateway'
+import TripleTitleSetContentSelection
+  from 'components/Widgets/User/TripleTitleSetPanel/TripleTitleSetContentSelection/TripleTitleSetContentSelection.vue'
 
 export default {
   name: 'AddLink',
+  components: { TripleTitleSetContentSelection },
   props: {
     value: {
       type: Array,
@@ -45,12 +49,34 @@ export default {
     return {
       content: null,
       contents: [],
-      localContents: [],
       loading: false
     }
   },
+  computed: {
+    localContents: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        // const contentsIds = []
+        // value.forEach(content => {
+        //   contentsIds.push(content.id)
+        // })
+        this.$emit('update:value', value)
+      }
+    }
+  },
+  watch: {
+    value (value) {
+      const contentsIds = []
+      value.forEach(content => {
+        contentsIds.push(content.id)
+      })
+      this.$emit('update:value', contentsIds)
+    }
+  },
   mounted () {
-    this.localContents = this.value
+    console.log(this.value)
     this.value.forEach(content => {
       this.contents.push(content.id)
     })
