cafdb89ecee7a40a5af9d7417964ea55ee213914
MJavad <javadhosseini14@gmail.com>
2023-12-04 12:22:32 +0330

    fix: Modam Font Issue Fixed!

--- src/components/Utils/Editor.vue
+++ src/components/Utils/Editor.vue
@@ -74,7 +74,7 @@ export default {
             icon: $q.iconSet.editor.font,
             list: 'no-icons',
             // options: ['p', 'h3', 'h4', 'h5', 'h6', 'code']
-            options: ['Doran', 'IRANSans', 'IRANSansDN', 'IRANSansX', 'iranyekan', 'Plasma', 'Panchang', 'Modam']
+            options: ['Doran', 'IRANSans', 'IRANSansDN', 'IRANSansX', 'iranyekan', 'Plasma', 'Panchang', 'ModamWeb', 'ModamFaNumWeb', 'ModamNoEnWeb']
           },
           {
             icon: $q.iconSet.editor.fontSize,
@@ -104,7 +104,9 @@ export default {
         iranyekan: 'iranyekan',
         Plasma: 'Plasma-Lozenge-Dots',
         Panchang: 'Panchang',
-        Modam: 'Modam'
+        ModamWeb: 'ModamWeb',
+        ModamFaNumWeb: 'ModamFaNumWeb',
+        ModamNoEnWeb: 'ModamNoEnWeb'
         // options: ['Doran', 'IRANSans', 'IRANSansDN', 'IRANSansX', 'iranyekan', 'Plasma']
       }
     }
@@ -125,8 +127,7 @@ export default {
   methods: {
     onPaste (evt) {
       const str = evt.clipboardData.getData('text')
-      const replace = str.replace(/(<([^>]+)>)/ig, '')
-      this.editorValue = replace
+      this.editorValue = str.replace(/(<([^>]+)>)/ig, '')
     },
     color (cmd, name) {
       const edit = this.$refs.editor
