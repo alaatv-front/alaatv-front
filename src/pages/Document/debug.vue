93b4d3c2e1f9c841fdeb536e4cac0358341bc95c
kerasus <aliesmaeeli20@gmail.com>
2023-12-11 13:08:26 +0330

    restore debug page

--- src/pages/Document/debug.vue
+++ src/pages/Document/debug.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <show-mothers-day-postcard />
+      <div class="row items-center justify-center">
+        <div class="col-6">
+          debug
+        </div>
+      </div>
     </div>
   </div>
 </template>
