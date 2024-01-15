2031a5664c41ff473eede4222dfa5dd5ce0b9a46
kerasus <aliesmaeeli20@gmail.com>
2024-01-15 16:59:56 +0330

    fix: remove unused isLandingPage.js middleware

--- src/router/middleware/middleware.js
+++ src/router/middleware/middleware.js
@@ -1,5 +1,4 @@
 import Authenticated from './Authenticated.js'
-import isLandingPage from './isLandingPage.js'
 import IncompleteProfile from './incompleteProfile.js'
 
-export { Authenticated, isLandingPage, IncompleteProfile }
+export { Authenticated, IncompleteProfile }
