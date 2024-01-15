a59b1e62849bfd93a178bdab370f2ac4d5bc68fb
ali <aliesmaeeli20@gmail.com>
2024-01-15 23:09:31 +0330

    fix: debug ttsp

--- src/layouts/TripleTitleSetLayout.vue
+++ src/layouts/TripleTitleSetLayout.vue
@@ -23,12 +23,12 @@
                  :to="{ name: 'UserPanel.Asset.TripleTitleSet.Products' }">بازگشت</q-btn>
         </div>
       </div>
-      <!--      <layout-menu :menu-key="menuKey"-->
-      <!--                   :topics-route-array="topicsRouteArray"-->
-      <!--                   :topic-list="topicList"-->
-      <!--                   :selected-topic="selectedTopic"-->
-      <!--                   :product-items="productItems"-->
-      <!--                   @item-selected="itemSelected" />-->
+      <layout-menu :menu-key="menuKey"
+                   :topics-route-array="topicsRouteArray"
+                   :topic-list="topicList"
+                   :selected-topic="selectedTopic"
+                   :product-items="productItems"
+                   @item-selected="itemSelected" />
     </div>
     <div class="container">
       <div class="header">
@@ -73,13 +73,13 @@ import { mapMutations } from 'vuex'
 import Router from 'src/router/Router.vue'
 import { Content } from 'src/models/Content.js'
 import KeepAliveComponents from 'src/assets/js/KeepAliveComponents.js'
-// import LayoutMenu from 'src/components/DashboardTripleTitleSet/LayoutMenu.vue'
+import LayoutMenu from 'src/components/DashboardTripleTitleSet/LayoutMenu.vue'
 
 export default {
   name: 'TripleTitleSetLayout',
   components: {
-    Router
-    // LayoutMenu
+    Router,
+    LayoutMenu
   },
   data () {
     return {
