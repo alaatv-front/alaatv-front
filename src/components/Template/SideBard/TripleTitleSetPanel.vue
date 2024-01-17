5ccfefa66a3712da70a59854531dedee7f03b6b7
kerasus <aliesmaeeli20@gmail.com>
2024-01-17 18:59:40 +0330

    ref: ttsp panel style

--- src/components/Template/SideBard/TripleTitleSetPanel.vue
+++ src/components/Template/SideBard/TripleTitleSetPanel.vue
@@ -264,6 +264,7 @@ export default {
     itemSelected (topic) {
       this.updateSelectedTopic(topic.title)
       this.updateLeftDrawer()
+      this.$store.commit('AppLayout/updateLayoutLeftDrawerVisible', false)
     }
   }
 }
