989ee04c7718ba0064dbd2fc9abf486aefb6fee1
kerasus <aliesmaeeli20@gmail.com>
2024-01-24 15:03:40 +0330

    fix: ttsp change selected product and selected topic

--- src/components/Widgets/User/TripleTitleSetPanel/TripleTitleSetProductPage/TripleTitleSetProductPage.vue
+++ src/components/Widgets/User/TripleTitleSetPanel/TripleTitleSetProductPage/TripleTitleSetProductPage.vue
@@ -150,7 +150,13 @@ export default {
         .then(() => {
           const newProductId = this.$route.params.productId
           const oldSelectedProduct = this.$store.getters['TripleTitleSet/selectedProduct']
-          if (!oldSelectedProduct?.id || parseInt(oldSelectedProduct.id) !== parseInt(newProductId)) {
+          const setList = this.$store.getters['TripleTitleSet/setList']
+          const setTopicList = this.$store.getters['TripleTitleSet/setTopicList']
+          if (
+            (!Array.isArray(setList) || setList.length === 0) ||
+            (!Array.isArray(setTopicList) || setTopicList.length === 0) ||
+            (!oldSelectedProduct?.id || parseInt(oldSelectedProduct.id) !== parseInt(newProductId))
+          ) {
             this.getProductSets(this.$route.params.productId)
           }
         })
