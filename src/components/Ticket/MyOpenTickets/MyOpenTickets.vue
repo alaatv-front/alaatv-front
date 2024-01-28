7ff4f68143ffa0f79de3d2b3ac99eb0a93d2eea4
Ayhanemoon <ayhan.samimi@example.com>
2024-01-28 15:34:05 +0330

    feat: my open ticket list added

---
+++ src/components/Ticket/MyOpenTickets/MyOpenTickets.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="my-tickets-container">
+    <div class="my-tickets-toolbar">
+      <q-input v-model="search"
+               type="text"
+               placeholder="جست و جو"
+               @keyup.enter="updateList">
+        <template #prepend>
+          <q-icon color="grey-6"
+                  name="ph:magnifying-glass" />
+        </template>
+      </q-input>
+      <q-separator class="my-tickets-toolbar__separator" />
+    </div>
+    <div class="my-tickets-list">
+      <ticket-item v-for="(ticket, index) in tickets"
+                   :key="index"
+                   :ticket="ticket" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TicketItem from './components/TicketItem.vue'
+
+export default {
+  name: 'MyOpenTickets',
+  components: {
+    TicketItem
+  },
+  emits: ['updateList'],
+  data () {
+    return {
+      search: '',
+      tickets: [
+        {
+          user: {
+            fullName: 'بهزاد آشفته',
+            image: 'https://nodes.alaatv.com/upload/alaaPages/2024-01/Avatar1706441404.png'
+          },
+          messages: 8
+        },
+        {
+          user: {
+            fullName: 'بهزاد آشفته',
+            image: 'https://nodes.alaatv.com/upload/alaaPages/2024-01/Avatar1706441404.png'
+          },
+          messages: 8
+        },
+        {
+          user: {
+            fullName: 'بهزاد آشفته',
+            image: 'https://nodes.alaatv.com/upload/alaaPages/2024-01/Avatar1706441404.png'
+          },
+          messages: 8
+        }
+      ]
+    }
+  },
+  methods: {
+    updateList () {
+      this.$emit('updateList', this.search)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-tickets{
+  &-container {
+    padding: $space-5 $spacing-none;
+  }
+  &-toolbar {
+    padding: $spacing-none $space-4;
+    &__separator {
+      margin: $space-3 $spacing-none;
+    }
+  }
+  &-list {
+    padding: $spacing-none $space-3;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: $space-3;
+    align-self: stretch;
+  }
+}
+</style>
---
+++ src/components/Ticket/MyOpenTickets/components/TicketItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="ticket-item-container">
+    <div class="ticket-item-avatar">
+      <q-avatar size="40px">
+        <lazy-img :src="ticket.user.image"
+                  width="40px"
+                  height="40px" /></q-avatar>
+      <div class="ticket-item-avatar__badge">
+        {{ticket.messages}}
+      </div>
+    </div>
+    <div class="ticket-item-description">
+      <div class="ticket-item-description__user">
+        {{ticket.user.fullName}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LazyImg from 'src/components/lazyImg.vue'
+export default {
+  name: 'TicketItem',
+  components: {
+    LazyImg
+  },
+  props: {
+    ticket: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .ticket-item {
+    &-container {
+      padding: $space-1;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 8px;
+      align-self: stretch;
+      transition: all ease-in-out .3s;
+
+      &:hover {
+        border-radius: 8px;
+        background: $grey-2;
+        padding: $space-2;
+        transition: all ease-in-out .3s;
+      }
+    }
+
+    &-avatar {
+      position: relative;
+      &__badge {
+        display: flex;
+        height: 16px;
+        min-width: 16px;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        bottom: 0px;
+        right: 0px;
+        border-radius: $radius-5;
+        background: $secondary;
+
+        color: $grey-1;
+        text-align: center;
+
+        @include caption2;
+      }
+    }
+
+    &-description {
+      width: calc(100% - 48px);
+
+      &__user {
+        color: $grey-9;
+        @include body2;
+      }
+    }
+  }
+</style>
