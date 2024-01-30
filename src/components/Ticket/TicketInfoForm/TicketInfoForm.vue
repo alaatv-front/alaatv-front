88d924dbb69e8b0fde7b015beba0c1fcbd7a12ef
Ayhanemoon <ayhan.samimi@example.com>
2024-01-30 12:50:27 +0330

    feat:  ticket info form component added

---
+++ src/components/Ticket/TicketInfoForm/TicketInfoForm.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="ticket-info-container">
+    <div class="ticket-info-header">
+      <div class="ticket-info-header__title">
+        عنوان :
+      </div>
+      <div class="ticket-info-header__text ellipsis-3-lines">
+        {{ ticket.title}}
+        <q-tooltip>
+          {{ ticket.title}}
+        </q-tooltip>
+      </div>
+    </div>
+    <q-separator class="ticket-info__separator" />
+    <div class="ticket-info-details">
+      <div class="ticket-info-details__item">
+        <div class="ticket-info-details__ticket-id__title">
+          شماره تیکت :
+        </div>
+        <div class="ticket-info-details__item__value">
+          {{ ticket.id}}
+        </div>
+      </div>
+      <div class="ticket-info-details__item">
+        <div class="ticket-info-details__item__title">
+          اولویت :
+        </div>
+        <div class="ticket-info-details__item__value">
+          {{ ticket.priority.title }}
+        </div>
+      </div>
+      <div class="ticket-info-details__item">
+        <div class="ticket-info-details__item__title">
+          ایجاد شده در :
+        </div>
+        <div class="ticket-info-details__item__value">
+          {{ ticket.shamsiDate('created_at').dateTime }}
+        </div>
+      </div>
+      <div class="ticket-info-details__item">
+        <div class="ticket-info-details__item__title">
+          آخرین به روز رسانی :
+        </div>
+        <div class="ticket-info-details__item__value">
+          {{ ticket.shamsiDate('updated_at').dateTime }}
+        </div>
+      </div>
+    </div>
+    <div class="ticket-info-form">
+      <entity-edit v-if="isEntityReady"
+                   ref="entityEdit"
+                   v-model:value="inputs"
+                   :show-save-button="false"
+                   :show-close-button="false"
+                   :defaultLayout="false"
+                   :loaded-data="ticket"
+                   :entity-id-key="entityIdKey">
+        <template #after-form-builder>
+          <q-btn color="secondary"
+                 icon="ph:pencil-simple"
+                 class="full-width q-mt-md"
+                 label="ویرایش"
+                 @click="onClick" />
+        </template>
+      </entity-edit>
+      <div v-else
+           class="ticket-info-form--loading">
+        <q-skeleton height="42px" />
+        <q-skeleton height="42px" />
+        <q-skeleton height="42px" />
+        <q-skeleton height="40px" />
+      </div>
+    </div>
+    <div class="ticket-info-action">
+      <q-btn color="primary"
+             class="full-width"
+             icon="ph:phone-outgoing"
+             label="تماس با کاربر"
+             @click="callUser" />
+      <q-btn outline
+             color="grey"
+             class="full-width"
+             label="ارسال پیامک"
+             @click="openSmsDialog" />
+    </div>
+    <q-dialog v-model="smsDialog">
+      <sms-dialog-inner />
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { EntityEdit } from 'quasar-crud'
+import { Ticket } from 'src/models/Ticket.js'
+import { mixinTicket } from 'src/mixin/Mixins.js'
+import SmsDialogInner from './components/SmsDialogInner.vue'
+
+export default {
+  name: 'TicketInfoForm',
+  components: {
+    EntityEdit,
+    SmsDialogInner
+  },
+  mixins: [mixinTicket],
+  props: {
+    ticket: {
+      type: Ticket,
+      default: new Ticket()
+    }
+  },
+  data () {
+    return {
+      isEntityReady: false,
+      smsDialog: false,
+      inputs: [
+        {
+          type: 'select',
+          name: 'department_id',
+          options: [],
+          optionLabel: 'title',
+          optionValue: 'id',
+          label: 'گروه',
+          disable: false,
+          col: 'col-12'
+        },
+        {
+          type: 'select',
+          name: 'status_id',
+          multiple: true,
+          options: [],
+          optionLabel: 'title',
+          optionValue: 'id',
+          label: 'وضعیت',
+          disable: false,
+          col: 'col-12'
+        },
+        {
+          type: 'select',
+          name: 'status',
+          options: [],
+          optionLabel: 'title',
+          optionValue: 'id',
+          responseKey: 'ticket.status.id',
+          label: 'پشتیبان',
+          disable: false,
+          col: 'col-12'
+        }
+      ],
+      entityIdKey: 'id',
+      entityParamKey: 'id'
+    }
+  },
+  methods: {
+    async setInputs () {
+      const ticketFields = await this.getTicketData()
+      this.getInput('department_id').options = ticketFields.departments.list
+      this.getInput('status_id').options = ticketFields.statuses.list
+    },
+    async initTicket () {
+      // this.setEntityValues()
+      await this.setInputs()
+      this.isEntityReady = true
+    },
+    openSmsDialog () {
+      this.smsDialog = true
+    },
+    callUser () {
+      alert('calling ...')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ticket-info {
+  &-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: $space-5;
+    align-self: stretch;
+  }
+
+  &-header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    color: $grey-9;
+    text-align: left;
+    @include body1;
+
+    &__text {
+      width: calc(100% - 64px);
+      margin-left: $space-1;
+    }
+  }
+
+  &__separator {
+    width: 100%;
+  }
+
+  &-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: $space-4;
+    align-self: stretch;
+
+    &__item{
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+      align-self: stretch;
+
+      &__title {
+        color: $grey-9;
+        @include body1;
+      }
+
+      &__value {
+        color: $grey-9;
+        @include body1;
+      }
+    }
+  }
+
+  &-form {
+    width: 100%;
+
+    &--loading {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: $space-5;
+    }
+  }
+
+  &-action {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: $space-4;
+    align-self: stretch;
+  }
+}
+</style>
---
+++ src/components/Ticket/TicketInfoForm/components/SmsDialogInner.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-card>
+    <q-card-section class="dialog-header-section">
+      <div class="row items-center justify-between">
+        <div>
+          <q-icon class="dialog-avatar"
+                  color="primary"
+                  size="sm"
+                  name="ph:chat-circle-text" />
+          ارسال پیامک به سید مرتضی صحیح النسب
+        </div>
+        <q-btn v-close-popup
+               icon="ph:x"
+               color="grey"
+               square
+               class="size-xs"
+               flat />
+      </div>
+    </q-card-section>
+    <q-separator class="grey1" />
+    <q-card-section class="dialog-body-section">
+      <q-select v-model="model"
+                :options="options"
+                label="نوع پیامک"
+                outlined />
+      <q-input v-model="input1"
+               class="smsTextarea"
+               label="ارسال پیام متنی"
+               placeholder="متن پیامک"
+               type="textarea" />
+    </q-card-section>
+    <q-card-section class="dialog-action-section">
+      <div class="row items-center justify-between">
+        <div />
+        <div class="dialog-action-btn-box">
+          <q-btn v-close-popup
+                 class="q-btn-md keep-min-width q-mr-sm"
+                 color="grey"
+                 size="md"
+                 outline>
+            انصراف
+          </q-btn>
+          <q-btn class="q-btn-md keep-min-width"
+                 color="primary">
+            ارسال
+          </q-btn>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SmsDialogInner'
+})
+</script>
+
+<style lang="scss" scoped>
+.smsTextarea {
+  margin-top: $space-6;
+}
+</style>
