12654a09d3348bd5ad5743a9a61f43afc0e44bc1
kerasus <aliesmaeeli20@gmail.com>
2024-02-03 19:52:40 +0330

    fix: TicketShow responsive

--- src/components/Widgets/Ticket/TicketShow/TicketShow.vue
+++ src/components/Widgets/Ticket/TicketShow/TicketShow.vue
@@ -22,18 +22,30 @@
         </q-breadcrumbs-el>
       </q-breadcrumbs>
     </div>
-    <div class="TicketShow__template">
-      <div class="TicketShow__header">
-        <ticket-header :ticket="ticket" />
+    <div class="TicketShow__template row">
+      <div class="col-lg-2 col-md-3 gt-sm">
+        <div class="TicketShow__my-open-tickets">
+          <my-open-tickets />
+        </div>
       </div>
-      <div class="TicketShow__my-open-tickets">
-        <my-open-tickets />
-      </div>
-      <div class="TicketShow__ticket-info">
-        <ticket-info-form :ticket="ticket" />
-      </div>
-      <div class="TicketShow__messages">
-        <ticket-message-list :ticket="ticket" />
+      <div class="col-lg-10 col-md-9 col-12">
+        <div class="row">
+          <div class="col-12">
+            <div class="TicketShow__header">
+              <ticket-header :ticket="ticket" />
+            </div>
+          </div>
+          <div class="col-lg-8 col-md-6 col-12">
+            <div class="TicketShow__messages">
+              <ticket-message-list :ticket="ticket" />
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-12 gt-sm">
+            <div class="TicketShow__ticket-info">
+              <ticket-info-form :ticket="ticket" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -111,6 +123,32 @@ export default {
               major: null
             },
             ticket_id: 191163,
+            body: 'این ته، دم در زندان قدم زدم و در دفتر بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این.',
+            files: {
+              photo: null,
+              voice: null,
+              file: null
+            },
+            is_private: 0,
+            report: {
+              has_reported: 0,
+              report_description: null
+            },
+            created_at: '2024-01-28 04:57:27'
+          },
+          {
+            id: 578122,
+            user: {
+              id: 1,
+              first_name: 'رجاء',
+              last_name: 'همدانی',
+              mobile: '09544659813',
+              national_code: '0000000000',
+              photo: 'https://nodes.alaatv.com/upload/images/profile/default_avatar.jpg',
+              role: 'پشتیبان',
+              major: null
+            },
+            ticket_id: 191163,
             body: 'این ته، دم در زندان قدم زدم و در دفتر بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این.',
             files: {
               photo: null,
@@ -127,7 +165,7 @@ export default {
           {
             id: 578123,
             user: {
-              id: 1,
+              id: 2,
               first_name: 'آفاق',
               last_name: 'کاشی',
               mobile: '09999999999',
@@ -138,6 +176,84 @@ export default {
             },
             ticket_id: 191163,
             body: 'به دستش دادم و موقع آن رسیده بود که چنین اهمیتی پیدا می‌کردم. این هم یک مزیت دیگر مدیری مدرسه بود! سی صد تومان.',
+            files: {
+              photo: null,
+              voice: null,
+              file: null
+            },
+            is_private: 0,
+            report: {
+              has_reported: 0,
+              report_description: null
+            },
+            created_at: '2024-01-28 04:57:27'
+          },
+          {
+            id: 578122,
+            user: {
+              id: 1,
+              first_name: 'رجاء',
+              last_name: 'همدانی',
+              mobile: '09544659813',
+              national_code: '0000000000',
+              photo: 'https://nodes.alaatv.com/upload/images/profile/default_avatar.jpg',
+              role: 'پشتیبان',
+              major: null
+            },
+            ticket_id: 191163,
+            body: 'این ته، دم در زندان قدم زدم و در دفتر بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این.',
+            files: {
+              photo: null,
+              voice: null,
+              file: null
+            },
+            is_private: 0,
+            report: {
+              has_reported: 0,
+              report_description: null
+            },
+            created_at: '2024-01-28 04:57:27'
+          },
+          {
+            id: 578122,
+            user: {
+              id: 1,
+              first_name: 'رجاء',
+              last_name: 'همدانی',
+              mobile: '09544659813',
+              national_code: '0000000000',
+              photo: 'https://nodes.alaatv.com/upload/images/profile/default_avatar.jpg',
+              role: 'پشتیبان',
+              major: null
+            },
+            ticket_id: 191163,
+            body: 'این ته، دم در زندان قدم زدم و در دفتر بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این.',
+            files: {
+              photo: null,
+              voice: null,
+              file: null
+            },
+            is_private: 0,
+            report: {
+              has_reported: 0,
+              report_description: null
+            },
+            created_at: '2024-01-28 04:57:27'
+          },
+          {
+            id: 578122,
+            user: {
+              id: 1,
+              first_name: 'رجاء',
+              last_name: 'همدانی',
+              mobile: '09544659813',
+              national_code: '0000000000',
+              photo: 'https://nodes.alaatv.com/upload/images/profile/default_avatar.jpg',
+              role: 'پشتیبان',
+              major: null
+            },
+            ticket_id: 191163,
+            body: 'این ته، دم در زندان قدم زدم و در دفتر بازرسی تصدیق کرد که این صدا را پای تخته سیاه خراب خواهد کرد. و گفتم: - این.',
             files: {
               photo: null,
               voice: null,
@@ -238,35 +354,19 @@ export default {
     margin-bottom: $space-6;
   }
   .TicketShow__template {
-    display: grid;
-    grid-template-columns: 184px auto 288px;
-    grid-template-rows: 72px auto;
-    grid-column-gap: 0;
-    grid-row-gap: 0;
     /* 1440 < page < 1920 */
     @include media-max-width('xl') {
-      grid-template-columns: 152px auto 240px;
     }
     /* 1024 < page < 1440 */
     @include media-max-width('lg') {
-      grid-template-columns: 161px auto 220px;
     }
     /* 600 < page < 1024 */
     @include media-max-width('md') {
-      .clickBodyMovin {
-        width: 160px;
-        bottom: 240px;
-      }
     }
     /* 360 < page < 600 */
     @include media-max-width('sm') {
-      .clickBodyMovin {
-        width: 128px;
-        bottom: 240px;
-      }
     }
     .TicketShow__header {
-      grid-area: 1 / 2 / 2 / 4;
       display: flex;
       padding: $space-3 $space-4;
       justify-content: space-between;
@@ -276,10 +376,14 @@ export default {
       border-bottom: 1px solid $blue-grey-3;
       border-radius: $radius-none $radius-4 $radius-none $radius-none;
       background: $grey-1;
+      /* 600 < page < 1024 */
+      @include media-max-width('md') {
+        border-radius: $radius-none !important;
+      }
     }
     .TicketShow__my-open-tickets {
-      grid-area: 1 / 1 / 3 / 2;
       display: flex;
+      height: 100%;
       padding: $space-5 $space-3;
       flex-direction: column;
       align-items: center;
@@ -289,8 +393,8 @@ export default {
       background: $grey-1;
     }
     .TicketShow__ticket-info {
-      grid-area: 2 / 3 / 3 / 4;
       display: flex;
+      height: 100%;
       padding: $space-6;
       flex-direction: column;
       align-items: center;
@@ -302,7 +406,8 @@ export default {
     }
     .TicketShow__messages {
       height: 100%;
-      grid-area: 2 / 2 / 3 / 3;
+      min-height: 100%;
+      max-height: 80vh;
     }
   }
 }
