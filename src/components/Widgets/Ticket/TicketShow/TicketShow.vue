f2bc2730a2b62492421f0af773da4b354a0a77ae
kerasus <aliesmaeeli20@gmail.com>
2024-02-04 16:31:37 +0330

    fix: TicketShow responsive

--- src/components/Widgets/Ticket/TicketShow/TicketShow.vue
+++ src/components/Widgets/Ticket/TicketShow/TicketShow.vue
@@ -33,7 +33,9 @@
         <div class="row">
           <div class="col-12">
             <div class="TicketShow__header">
-              <ticket-header :ticket="ticket" />
+              <ticket-header :ticket="ticket"
+                             @openTickets="openMyOpenTicketDrawer"
+                             @openInfoForm="openTicketInfoFormDrawer" />
             </div>
           </div>
           <div class="col-lg-8 col-md-6 col-12">
