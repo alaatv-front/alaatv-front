9423ccffd72527e85bce013e6329f7f1f8c66cbc
kerasus <aliesmaeeli20@gmail.com>
2024-02-14 13:41:27 +0330

    feat: scrollToBottom after 1000ms

--- src/components/Ticket/TicketMessageList/TicketMessageList.vue
+++ src/components/Ticket/TicketMessageList/TicketMessageList.vue
@@ -65,7 +65,9 @@ export default defineComponent({
     }
   },
   mounted () {
-    this.scrollToBottom()
+    setTimeout(() => {
+      this.scrollToBottom()
+    }, 1000)
   },
   methods: {
     acceptTicket () {
