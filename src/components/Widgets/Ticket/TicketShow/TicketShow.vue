c27e0637faf5a1d43c9c6acafa67863c671adea9
kerasus <aliesmaeeli20@gmail.com>
2024-03-05 13:44:17 +0330

    fix: ticket show mobile responsive

--- src/components/Widgets/Ticket/TicketShow/TicketShow.vue
+++ src/components/Widgets/Ticket/TicketShow/TicketShow.vue
@@ -555,7 +555,7 @@ export default {
       max-height: calc( 100vh - 300px);
       /* 600 < page < 1024 */
       @include media-max-width('md') {
-        min-height: calc( 100vh - 300px);
+        min-height: calc( 100vh - 360px);
         max-height: calc( 100vh - 194px);
       }
     }
