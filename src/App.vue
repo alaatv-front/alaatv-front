6144604e52e45649e782f752f3643ff6bfccbcc1
kerasus <aliesmaeeli20@gmail.com>
2024-03-18 18:23:19 +0330

    fix: android app deep link

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,30 @@
 </template>
 
 <script>
+import { App } from '@capacitor/app'
 import { defineComponent } from 'vue'
+import { Capacitor } from '@capacitor/core'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  mounted () {
+    if (Capacitor.isNativePlatform()) {
+      this.setEventListenerForNativeAppDeepLink()
+    }
+  },
+  methods: {
+    setEventListenerForNativeAppDeepLink () {
+      App.addListener('appUrlOpen', function (event) {
+        // Example url: https://beerswift.app/tabs/tabs2
+        // slug = /tabs/tabs2
+        const urlObject = new URL(event.url)
+        const path = urlObject.href.replace(urlObject.origin, '')
+        // We only push to the route if there is a slug present
+        if (path) {
+          this.$router.push({ path })
+        }
+      })
+    }
+  }
 })
 </script>
