d64b955c769d6469259087fc0209d76bad4a199c
kerasus <aliesmaeeli20@gmail.com>
2024-03-18 13:06:40 +0330

    fix: debug ThankYouPage.vue for android app

--- src/components/Widgets/Cart/ThankYouPage/ThankYouPage.vue
+++ src/components/Widgets/Cart/ThankYouPage/ThankYouPage.vue
@@ -87,7 +87,7 @@ export default {
     }
     if (Capacitor.isNativePlatform() && window.location.href.indexOf('localhost') === -1) {
       alert('window.location.href -> (' + window.location.href + ')')
-      const replaced = window.location.href.replace('https://alaatv.com', 'https://localhost/#/')
+      const replaced = window.location.href.replace('https://alaatv.com', 'https://localhost/#')
       alert('replaced -> (' + replaced + ')')
       document.location = replaced
 
